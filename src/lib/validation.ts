b3a15e5c18a454b992bd2648c288fee359b301d0
JereUR <jeremias.jdv@gmail.com>
2024-08-14 03:04:43 -0300

    feat: Validations for signup and login added

---
+++ src/lib/validation.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+const requiredString = z.string().trim().min(1, "Requerido");
+
+export const signUpSchema = z.object({
+  email: requiredString.email("Email inválido."),
+  username: requiredString.regex(
+    /^[a-zA-Z0-9_-]+$/,
+    "Solo esta permitido palabras, números, _ y -.",
+  ),
+  password: requiredString.min(
+    8,
+    "Contraseña debe tener al menos 8 caracteres.",
+  ),
+});
+
+export type SignUpValues = z.infer<typeof signUpSchema>;
+
+export const loginSchema = z.object({
+  username: requiredString,
+  password: requiredString,
+});
+
+export type LoginValues = z.infer<typeof loginSchema>;
