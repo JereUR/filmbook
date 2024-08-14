a86ea468b1e4315f8154bcfc9e2305978e1e378e
JereUR <jeremias.jdv@gmail.com>
2024-08-14 03:34:55 -0300

    feat: Login action added

---
+++ src/app/(auth)/login/actions.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import { isRedirectError } from "next/dist/client/components/redirect";
+import { verify } from "@node-rs/argon2";
+import { lucia } from "@/auth";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+import prisma from "@/lib/prisma";
+import { loginSchema, LoginValues } from "@/lib/validation";
+
+export async function login(
+  credentials: LoginValues,
+): Promise<{ error: string }> {
+  try {
+    const { username, password } = loginSchema.parse(credentials);
+
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (!existingUser || !existingUser.passwordHash) {
+      return { error: "Credenciales incorrectas." };
+    }
+
+    const validPassword = await verify(existingUser.passwordHash, password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    });
+
+    if (!validPassword) {
+      return { error: "Credenciales incorrectas." };
+    }
+
+    const session = await lucia.createSession(existingUser.id, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes,
+    );
+
+    return redirect("/");
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+    console.error(error);
+    return { error: "Algo salió mal. Por favor inténtalo de nuevo." };
+  }
+}
