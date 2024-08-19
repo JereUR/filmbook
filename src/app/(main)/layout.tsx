76777a9ce32824f26d0430f71c2bd6d7ad60bdfc
JereUR <jeremias.jdv@gmail.com>
2024-08-18 21:58:33 -0300

    feat: Menu bar responsive added

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { validateRequest } from "@/auth";
 import SessionProvider from "./SessionProvider";
 import Navbar from "./Navbar";
+import MenuBar from "./MenuBar";
 
 export default async function Layout({
   children,
@@ -12,13 +13,17 @@ export default async function Layout({
 }) {
   const session = await validateRequest();
 
-  if (!session.user) redirect("/login");
+  if (!session.user) redirect("/iniciar-sesion");
 
   return (
     <SessionProvider value={session}>
       <div className="flex min-h-screen flex-col">
         <Navbar />
-        <div className="mx-auto max-w-7xl p-5">{children}</div>
+        <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+          <MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
+          {children}
+        </div>
+        <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
       </div>
     </SessionProvider>
   );
