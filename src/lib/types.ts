7d87f1fb1da830123b0a518422a80239b2d49a34
JereUR <jeremias.jdv@gmail.com>
2024-08-19 22:35:00 -0300

    feat: Types for get/return data from prisma added

---
+++ src/lib/types.ts
@@ -0,0 +1,18 @@
+import { Prisma } from "@prisma/client";
+
+export const userDataselect = {
+  id: true,
+  username: true,
+  displayName: true,
+  avatarUrl: true,
+} satisfies Prisma.UserSelect;
+
+export const postDataInclude = {
+  user: {
+    select: userDataselect,
+  },
+} satisfies Prisma.PostInclude;
+
+export type PostData = Prisma.PostGetPayload<{
+  include: typeof postDataInclude;
+}>;
