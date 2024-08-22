94bb42e3c62ba76a494bfe8ce79e966e432c5e9e
JereUR <jeremias.jdv@gmail.com>
2024-08-21 21:53:50 -0300

    feat: Sintaxis changes

--- src/app/(main)/ForYouFeed.tsx
+++ src/app/(main)/ForYouFeed.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Loader2 } from "lucide-react";
+import React from "react";
+
 import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
 import Post from "@/components/posts/Post";
 import PostsLoadingSkeleton from "@/components/posts/PostsLoadingSkeleton";
 import kyInstance from "@/lib/ky";
 import { PostsPage } from "@/lib/types";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { Loader2 } from "lucide-react";
-import React from "react";
 
 export default function ForYouFeed() {
   const {
