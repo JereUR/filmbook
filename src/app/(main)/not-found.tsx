3c776bd4c8fb82c30c6187e156eccd52ee38a819
JereUR <jeremias.jdv@gmail.com>
2024-08-23 22:50:16 -0300

    feat: User not found page added

---
+++ src/app/(main)/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="my-12 w-full space-y-3 text-center">
+      <h1 className="text-3xl font-bold">No encontrado</h1>
+      <p>La página que buscas no existe.</p>
+    </main>
+  );
+}
