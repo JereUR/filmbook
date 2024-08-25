cb0b5783827fe911f1d1ad7de9289982d45cb0c4
JereUR <jeremias.jdv@gmail.com>
2024-08-25 02:50:53 -0300

    feat: Uploadthing condigured

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,35 @@
+import { createUploadthing, FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+
+const f = createUploadthing();
+
+export const fileRoute = {
+  avatar: f({
+    image: { maxFileSize: "512KB" },
+  })
+    .middleware(async () => {
+      const { user } = await validateRequest();
+
+      if (!user) throw new UploadThingError("No autorizado");
+
+      return { user };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      const newAvatarUrl = file.url.replace(
+        "/f/",
+        `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}`,
+      );
+
+      await prisma.user.update({
+        where: { id: metadata.user.id },
+        data: { avatarUrl: newAvatarUrl },
+      });
+
+      return { avatarUrl: newAvatarUrl };
+    }),
+} satisfies FileRouter;
+
+export type AppFileRoute = typeof fileRoute;
