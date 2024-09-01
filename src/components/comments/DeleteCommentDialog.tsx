c414d48381244a0dd01b1b060374b379eefa5676
JereUR <jeremias.jdv@gmail.com>
2024-09-01 09:12:31 -0300

    feat: UI for delete comment added

---
+++ src/components/comments/DeleteCommentDialog.tsx
@@ -0,0 +1,62 @@
+import { CommentData } from "@/lib/types";
+import { useDeleteCommentMutation } from "./mutations";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
+import LoadingButton from "../LoadingButton";
+import { Button } from "../ui/button";
+
+interface DeleteCommentDialogProps {
+  comment: CommentData;
+  open: boolean;
+  onClose: () => void;
+}
+
+export default function DeleteCommentDialog({
+  comment,
+  open,
+  onClose,
+}: DeleteCommentDialogProps) {
+  const mutation = useDeleteCommentMutation();
+
+  function handleOpenChange(open: boolean) {
+    if (!open || !mutation.isPending) {
+      onClose();
+    }
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={handleOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Borrar comentario?</DialogTitle>
+          <DialogDescription>
+            Estas seguro que quieres eliminar este comentario? Esta acción no se
+            puede deshacer.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <LoadingButton
+            variant="destructive"
+            onClick={() => mutation.mutate(comment.id, { onSuccess: onClose })}
+            loading={mutation.isPending}
+          >
+            Borrar
+          </LoadingButton>
+          <Button
+            variant="outline"
+            onClick={onClose}
+            disabled={mutation.isPending}
+          >
+            Cancelar
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
