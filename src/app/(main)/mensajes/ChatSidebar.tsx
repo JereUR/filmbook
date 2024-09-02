7593073dbf22a915506d7e09fcd80664ddc9fa49
JereUR <jeremias.jdv@gmail.com>
2024-09-02 04:14:44 -0300

    feat: Chat sidebar component created

---
+++ src/app/(main)/mensajes/ChatSidebar.tsx
@@ -0,0 +1,13 @@
+import { ChannelList } from "stream-chat-react";
+
+import { useSession } from "../SessionProvider";
+
+export default function ChatSidebar() {
+  const { user } = useSession();
+
+  return (
+    <div className="flex size-full flex-col border-e md:w-72">
+      <ChannelList />
+    </div>
+  );
+}
