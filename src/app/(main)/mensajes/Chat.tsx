2ccf7ca6d1fcbd7299af83c1411ed6aa67b4887b
JereUR <jeremias.jdv@gmail.com>
2024-09-03 21:23:03 -0300

    feat: Theme addapted

--- src/app/(main)/mensajes/Chat.tsx
+++ src/app/(main)/mensajes/Chat.tsx
@@ -6,16 +6,26 @@ import { Chat as StreamChat } from "stream-chat-react";
 import useInitializeChatClient from "./useInitializeChatClient";
 import ChatSidebar from "./ChatSidebar";
 import ChatChannel from "./ChatChannel";
+import { useTheme } from "next-themes";
 
 export default function Chat() {
   const chatClient = useInitializeChatClient();
+
+  const { resolvedTheme } = useTheme();
 
   if (!chatClient) return <Loader2 className="mx-auto my-3 animate-spin" />;
 
   return (
     <main className="relative w-full overflow-hidden rounded-2xl bg-card shadow-sm">
       <div className="absolute bottom-0 top-0 flex w-full">
-        <StreamChat client={chatClient}>
+        <StreamChat
+          client={chatClient}
+          theme={
+            resolvedTheme === "dark"
+              ? "str-chat__theme-dark"
+              : "str-chat__theme-light"
+          }
+        >
           <ChatSidebar />
           <ChatChannel />
         </StreamChat>
