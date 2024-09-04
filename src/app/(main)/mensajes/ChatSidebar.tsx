e3e1a38029e7b678bbde81c1eae1c06b4a40034e
JereUR <jeremias.jdv@gmail.com>
2024-09-03 22:50:57 -0300

    feat: Custom menu header and new chat action started

--- src/app/(main)/mensajes/ChatSidebar.tsx
+++ src/app/(main)/mensajes/ChatSidebar.tsx
@@ -1,25 +1,46 @@
-import { ChannelList } from "stream-chat-react";
+import {
+  ChannelList,
+  ChannelPreviewMessenger,
+  ChannelPreviewUIComponentProps,
+} from "stream-chat-react";
 import { cn } from "@/lib/utils";
 
-import { X } from "lucide-react";
+import { MailPlus, X } from "lucide-react";
 
 import { useSession } from "../SessionProvider";
 import { Button } from "@/components/ui/button";
+import { useCallback, useState } from "react";
+import NewChatDialog from "./NewChatDialog";
 
-interface ChatSidebarProps{
-  open:boolean
-  onClose:()=>void
+interface ChatSidebarProps {
+  open: boolean;
+  onClose: () => void;
 }
 
-export default function ChatSidebar({
-  open,
-  onClose,
-}:ChatSidebarProps) {
+export default function ChatSidebar({ open, onClose }: ChatSidebarProps) {
   const { user } = useSession();
 
+  const ChannelPreviewCustom = useCallback(
+    (props: ChannelPreviewUIComponentProps) => (
+      <ChannelPreviewMessenger
+        {...props}
+        onSelect={() => {
+          props.setActiveChannel?.(props.channel, props.watchers);
+          onClose();
+        }}
+      />
+    ),
+    [onClose],
+  );
+
   return (
-    <div className={cn("md:flex size-full flex-col border-e md:w-72",open?'flex':'hidden')}>
-      <MenuHeader onClose={onClose}/>
+    <div
+      className={cn(
+        "size-full flex-col border-e md:flex md:w-72",
+        open ? "flex" : "hidden",
+      )}
+    >
+      <MenuHeader onClose={onClose} />
       <ChannelList
         filters={{
           type: "messaging",
@@ -36,21 +57,46 @@ export default function ChatSidebar({
             },
           },
         }}
+        Preview={ChannelPreviewCustom}
       />
     </div>
   );
 }
 
-interface MenuHeaderProps{
-  onClose:()=>void
+interface MenuHeaderProps {
+  onClose: () => void;
 }
 
-function MenuHeader({onClose}:MenuHeaderProps){
-  return <div className='flex items-center gap-3 p-2'>
-    <div className='h-full md:hidden'>
-      <Button size='icon' variant='ghost' onClick={onClose}>
-        <X className="size-5"/>
-      </Button>
-    </div>
-  </div>
+function MenuHeader({ onClose }: MenuHeaderProps) {
+  const [showNewChatDialog, setShowNewChatDialog] = useState<boolean>(false);
+
+  return (
+    <>
+      <div className="flex items-center gap-3 p-2">
+        <div className="h-full md:hidden">
+          <Button size="icon" variant="ghost" onClick={onClose}>
+            <X className="size-5" />
+          </Button>
+        </div>
+        <h1 className="me-auto text-xl font-bold md:ms-2">Mensajes</h1>
+        <Button
+          size="icon"
+          variant="ghost"
+          title="Iniciar nueva conversación"
+          onClick={() => setShowNewChatDialog(true)}
+        >
+          <MailPlus className="size-5" />
+        </Button>
+      </div>
+      {showNewChatDialog && (
+        <NewChatDialog
+          onOpenChange={setShowNewChatDialog}
+          onChatCreated={() => {
+            setShowNewChatDialog(false);
+            onClose();
+          }}
+        />
+      )}
+    </>
+  );
 }
