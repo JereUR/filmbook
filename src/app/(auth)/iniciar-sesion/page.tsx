e09e79e3fa21e85bc404155d26833b98b1fbee4e
JereUR <jeremias.jdv@gmail.com>
2024-09-05 04:03:29 -0300

    feat: Option to sign in with google added

--- src/app/(auth)/iniciar-sesion/page.tsx
+++ src/app/(auth)/iniciar-sesion/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import LoginForm from "./LoginForm";
 import loginImage from "@/assets/login-image.jpg";
 import Image from "next/image";
+import GoogleSignInButton from "./google/GoogleSignInButton";
 
 export const metadata: Metadata = {
   title: "Iniciar sesión",
@@ -19,10 +20,16 @@ export default function LoginPage() {
         <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
           <h1 className="text-center text-3xl font-bold">Iniciar Sesión</h1>
           <div className="space-y-5">
+            <GoogleSignInButton />
+            <div className="flex items-center gap-3">
+              <div className="h-px flex-1 bg-muted" />
+              <span className='text-primary'>ó</span>
+              <div className="h-px flex-1 bg-muted" />
+            </div>
             <LoginForm />
             <Link
               href="/registrarse"
-              className="block text-center text-purple-600 hover:text-purple-700 hover:underline"
+              className="block text-center text-primary hover:text-primary/70 hover:underline"
             >
               ¿No tienes una cuenta? Regístrate aquí.
             </Link>
