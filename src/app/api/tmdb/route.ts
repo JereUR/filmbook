cc2b07cbc4c76137319c61ecad9942af12020753
JereUR <jeremias.jdv@gmail.com>
2024-09-06 22:26:12 -0300

    feat: Route for search movie from title

---
+++ src/app/api/tmdb/route.ts
@@ -0,0 +1,40 @@
+// /app/api/tmdb/route.ts
+import { NextRequest } from "next/server";
+import { TMDBResponse } from "@/lib/types";
+
+export async function GET(req: NextRequest) {
+  const title = req.nextUrl.searchParams.get("title") || "";
+  const apiKey = process.env.MOVIE_API_KEY;
+  const accessToken = process.env.MOVIE_ACCESS_TOKEN;
+
+  if (!title || typeof title !== "string") {
+    return new Response(JSON.stringify({ error: "Parametro sin valor." }), {
+      status: 400,
+    });
+  }
+
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(title)}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json;charset=utf-8",
+        },
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error al buscar películas: ${response.statusText}`);
+    }
+
+    const data: TMDBResponse = await response.json();
+    return new Response(JSON.stringify(data));
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({ error: "Error Interno del Servidor." }),
+      { status: 500 },
+    );
+  }
+}
