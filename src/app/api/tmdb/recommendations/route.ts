c8194786108e7951f0dd22599793aa414627d17c
JereUR <jeremias.jdv@gmail.com>
2024-09-10 23:16:56 -0300

    feat: Ruta para obtener recomendaciones para una pelicula en particular

---
+++ src/app/api/tmdb/recommendations/route.ts
@@ -0,0 +1,63 @@
+import { NextRequest, NextResponse } from "next/server";
+import { subWeeks, isAfter } from "date-fns";
+
+import prisma from "@/lib/prisma";
+import { fetchMovieRecommendations } from "@/lib/tmdb";
+
+export async function GET(req: NextRequest) {
+  const movieId = req.nextUrl.searchParams.get("id") || "";
+
+  if (!movieId) {
+    return NextResponse.json(
+      { error: "ID de película no proporcionado" },
+      { status: 400 },
+    );
+  }
+
+  let movie = await prisma.movie.findUnique({
+    where: { id: movieId },
+    select: {
+      id: true,
+      recommendations: true,
+      updatedAt: true,
+    },
+  });
+
+  if (movie && movie.recommendations !== null) {
+    const oneWeekAgo = subWeeks(new Date(), 1);
+
+    if (isAfter(oneWeekAgo, movie.updatedAt)) {
+      const recommendations = await fetchMovieRecommendations(movieId);
+      const recommendationsData=recommendations.results
+
+      const movie = await prisma.movie.update({
+        where: { id: movieId },
+        data: {
+          recommendations:recommendationsData,
+        },
+      });
+    }
+
+    return NextResponse.json(movie.recommendations);
+  }
+
+  try {
+    const recommendations = await fetchMovieRecommendations(movieId);
+    const recommendationsData=recommendations.results
+
+    const movie = await prisma.movie.update({
+      where: { id: movieId },
+      data: {
+        recommendations:recommendationsData,
+      },
+    });
+
+    return NextResponse.json(movie.recommendations);
+  } catch (error) {
+    console.error("Error al obtener las recomendaciones desde TMDB", error);
+    return NextResponse.json(
+      { error: "Error al obtener las recomendaciones" },
+      { status: 500 },
+    );
+  }
+}
