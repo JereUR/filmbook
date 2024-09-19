c4a8e7c8a52c1c1ebd95156be8297049bcfd1626
JereUR <jeremias.jdv@gmail.com>
2024-09-18 23:08:55 -0300

    feat: Estilos editados

--- src/components/movies/RatingsSection.tsx
+++ src/components/movies/RatingsSection.tsx
@@ -19,73 +19,79 @@ export default function RatingsSection({
       return "text-red-600";
     } else if (rating < 6.0) {
       return "text-yellow-600";
-    } else if(rating<9.0){
+    } else if (rating < 9.0) {
       return "text-green-600";
-    }else{
+    } else {
       return "text-primary";
     }
   }
 
   return (
-    <div className="w-full my-2 md:my-4 md:w-1/4 flex flex-col gap-2 rounded-2xl border border-primary/50 p-2 md:p-6">
-      <div className="flex gap-4 items-center justify-center">
-        <h1 className="text-lg md:text-xl font-bold">RATING</h1>
+    <div className="my-2 flex w-full flex-col gap-2 rounded-2xl border border-primary/50 p-2 md:my-4 md:w-1/4 md:p-4">
+      <div className="flex items-center justify-center gap-4">
+        <h1 className="font-semibold md:text-lg">RATING</h1>
       </div>
-      <div className="flex md:flex-col gap-2">
-      <div className="flex md:flex-col pr-4 border-r md:pr-0 md:border-r-0 md:pb-4 md:border-b">
-        <div className="flex items-center gap-4">
-          <div className="relative h-8 w-8 flex-shrink-0 md:h-14 md:w-14">
-            <Image
-              src={TmdbLogo}
-              alt="TMDB logo"
-              fill
-              sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw"
-              className="rounded"
-            />
-          </div>
-          <div className="flex flex-col items-center justify-center w-full">
-            {voteAverage ? (
-              <div className="flex gap-1 text-2xl italic">
-                <span className={ratingColor(voteAverage)}>{voteAverage.toFixed(1)}</span>
-                <span className="text-gray-400">/10</span>
-              </div>
-            ) : (
-              <span className="text-2xl text-center text-foreground/40">S/P</span>
-            )}
-            <span className="text-xs text-muted-foreground">
-              Votos: {voteCount ? voteCount : "-"}
-            </span>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className="flex items-center gap-4 pl-1 md:pl-0 md:pt-1">
-          <div className="relative h-8 w-8 flex-shrink-0 md:h-14 md:w-14">
-            <Image
-              src={AppLogo}
-              alt="Filmbook logo"
-              fill
-              sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw"
-              className="rounded"
-            />
-          </div>
-          <div className="flex flex-col items-center justify-center w-full">
-            {rating ? (
-              <div className="flex gap-1 text-2xl italic">
-                <span className={ratingColor(rating.averageRating)}>
-                  {rating.averageRating.toFixed(1)}
+      <div className="flex gap-2 md:flex-col justify-center">
+        <div className="flex border-r pr-8 md:flex-col md:border-b md:border-r-0 md:pb-4 md:pr-0">
+          <div className="flex items-center gap-4">
+            <div className="relative h-10 w-10 flex-shrink-0 md:h-14 md:w-14">
+              <Image
+                src={TmdbLogo}
+                alt="TMDB logo"
+                fill
+                sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw"
+                className="rounded"
+              />
+            </div>
+            <div className="flex w-full flex-col items-center justify-center">
+              {voteAverage ? (
+                <div className="flex gap-1 text-xl md:text-2xl italic font-semibold">
+                  <span className={ratingColor(voteAverage)}>
+                    {voteAverage.toFixed(1)}
+                  </span>
+                  <span className="text-gray-400">/10</span>
+                </div>
+              ) : (
+                <span className="text-center text-xl md:text-2xl text-foreground/40">
+                  S/P
                 </span>
-                <span className="text-gray-400">/5</span>
-              </div>
-            ) : (
-              <span className="text-2xl text-center text-foreground/40">S/P</span>
-            )}
-            <p className="text-xs text-muted-foreground">
-              Votos: {rating ? rating.numberOfRatings : "-"}
-            </p>
+              )}
+              <span className="text-xs text-foreground/40">
+                Votos: {voteCount ? voteCount : "-"}
+              </span>
+            </div>
           </div>
         </div>
-      </div>
+        <div>
+          <div className="flex items-center gap-4 pl-5 md:pl-0 md:pt-1">
+            <div className="relative h-8 w-8 flex-shrink-0 md:h-12 md:w-12">
+              <Image
+                src={AppLogo}
+                alt="Filmbook logo"
+                fill
+                sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw"
+                className="rounded"
+              />
+            </div>
+            <div className="flex w-full flex-col items-center justify-center">
+              {rating ? (
+                <div className="flex gap-1 text-xl md:text-2xl italic font-semibold">
+                  <span className={ratingColor(rating.averageRating)}>
+                    {rating.averageRating.toFixed(1)}
+                  </span>
+                  <span className="text-gray-400">/5</span>
+                </div>
+              ) : (
+                <span className="text-center text-xl md:text-2xl text-foreground/40">
+                  S/P
+                </span>
+              )}
+              <p className="text-xs text-foreground/40">
+                Votos: {rating ? rating.numberOfRatings : "-"}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
