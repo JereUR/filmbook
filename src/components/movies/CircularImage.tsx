90713d52be8c9dc90f29d79579faac9803a9cdbb
JereUR <jeremias.jdv@gmail.com>
2024-09-19 03:10:29 -0300

    feat: Contemplar que la imagen que se pase no exista

--- src/components/movies/CircularImage.tsx
+++ src/components/movies/CircularImage.tsx
@@ -1,26 +1,35 @@
 import Image from "next/image";
 
 import { cn } from "@/lib/utils";
+import noImage from "@/assets/avatar-placeholder.png";
 
 interface CircularImageProps {
   src: string;
   size?: number;
   alt: string;
-  className?:string
+  className?: string;
 }
 
-export default function CircularImage({ src, size, alt, className }: CircularImageProps) {
+export default function CircularImage({
+  src,
+  size,
+  alt,
+  className,
+}: CircularImageProps) {
   return (
     <div
       style={{ width: `${size ?? 38}px`, height: `${size ?? 38}px` }}
-      className={cn("overflow-hidden rounded-full ring ring-primary/50",className)}
+      className={cn(
+        "overflow-hidden rounded-full ring ring-primary/50",
+        className,
+      )}
     >
       <Image
-        src={src}
+        src={src ? src : noImage}
         width={size ?? 38}
         height={size ?? 38}
         alt={alt}
-        className="object-cover transform translate-y-[-10%]"
+        className={`object-cover ${src && "translate-y-[-10%] transform"}`}
       />
     </div>
   );
