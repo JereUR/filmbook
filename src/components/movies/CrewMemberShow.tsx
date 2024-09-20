690c55e1de8de6bcfb6bef1fc90aa25c48439d2a
JereUR <jeremias.jdv@gmail.com>
2024-09-20 04:09:32 -0300

    feat: Imagen aumentada

--- src/components/movies/CrewMemberShow.tsx
+++ src/components/movies/CrewMemberShow.tsx
@@ -8,7 +8,7 @@ interface CrewMemberShowProps {
 export default function CrewMemberShow({ member}: CrewMemberShowProps) {
   return (
     <div className="flex items-center gap-2 md:gap-3">
-      <CircularImage src={member.profilePath} alt={`${member.name} avatar`} size={36}/>
+      <CircularImage src={member.profilePath} alt={`${member.name} avatar`} size={40}/>
       <div className="flex flex-col">
         <span className="text-sm text-muted-foreground md:text-base">
           {member.name}
