bd344ed1e3189cc8d6edeef2faf4d182a05cf323
JereUR <jeremias.jdv@gmail.com>
2024-09-22 01:54:37 -0300

    feat: Modal para mostrar imagen en tamaño grande agregado

--- src/components/movies/CastMemberShow.tsx
+++ src/components/movies/CastMemberShow.tsx
@@ -1,14 +1,15 @@
-import type { CastMember } from "@/lib/types";
+import type { CastMember, ImageInfo } from "@/lib/types";
 import CircularImage from "./CircularImage";
 
 interface CastMemberShowProps {
   member: CastMember;
+  handleImageClick:(image:ImageInfo)=>void
 }
 
-export default function CastMemberShow({ member}: CastMemberShowProps) {
+export default function CastMemberShow({ member,handleImageClick}: CastMemberShowProps) {
   return (
     <div className="flex items-center gap-2 md:gap-3 min-h-[70px]">
-      <CircularImage src={member.profilePath} alt={`${member.name} avatar`} size={40}/>
+      <CircularImage src={member.profilePath} alt={`${member.name} avatar`} size={40} handleImageClick={handleImageClick}/>
       <div className="flex flex-col">
         <span className="text-sm text-muted-foreground md:text-base">
           {member.name}
