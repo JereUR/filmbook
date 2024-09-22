452c8d9306a1bb2aaf62c4fccf9292e65188642c
JereUR <jeremias.jdv@gmail.com>
2024-09-22 01:54:00 -0300

    feat: Ancho de imagen desde api en valor original siempre

--- src/app/api/tmdb/movie/route.ts
+++ src/app/api/tmdb/movie/route.ts
@@ -5,7 +5,7 @@ import prisma from "@/lib/prisma";
 import { fetchMovieFromTMDB } from "@/lib/tmdb";
 import { Movie } from "@/lib/types";
 
-const BASE_IMG_TMDB = "https://image.tmdb.org/t/p/w500";
+const BASE_IMG_TMDB = "https://image.tmdb.org/t/p/original";
 
 export async function GET(req: NextRequest) {
   const movieId = req.nextUrl.searchParams.get("id") || "";
