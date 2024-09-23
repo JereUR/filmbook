173e028c25959b3860fc9663c442e4339d545d5a
JereUR <jeremias.jdv@gmail.com>
2024-09-22 21:44:03 -0300

    feat: Mostrar rating de fuente en componente global

---
+++ src/components/movies/rating/RatingsSection.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Clapperboard, BadgePlus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import ShowAppRating from "./ShowAppRating";
+import { useState } from "react";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import RatingEditor from "./RatingEditor";
+
+interface RatingsSectionProps {
+  rating: any;
+  voteAverage?: number;
+  voteCount?: number;
+}
+
+export default function RatingsSection({
+  rating,
+  voteAverage,
+  voteCount,
+}: RatingsSectionProps) {
+  const [showRatingEditor, setShowRatingEditor] = useState<boolean>(false);
+
+  return (
+    <div className="my-2 flex w-full flex-col gap-4 rounded-2xl border border-primary/50 p-2 md:my-4 md:w-1/4 md:gap-3 md:p-4">
+      <div className="flex items-center justify-around gap-4">
+        <h1 className="text-lg font-semibold md:text-xl">RATING</h1>
+        <Button
+          variant="ghost"
+          className="group flex items-start gap-2 rounded-2xl border border-primary p-2 text-sm hover:bg-primary/80"
+          onClick={()=>setShowRatingEditor(true)}
+        >
+          <Clapperboard className="h-6 w-6" />
+          <BadgePlus className="h-[14px] w-[14px] transition-transform duration-300 ease-in-out group-hover:scale-125" />
+        </Button>
+      </div>
+      <div className="flex justify-center gap-2 md:flex-col">
+        <div className="flex border-r pr-8 md:flex-col md:border-b md:border-r-0 md:pb-4 md:pr-0">
+          <ShowAppRating
+            ownApp={false}
+            voteAverage={voteAverage}
+            voteCount={voteCount}
+          />
+        </div>
+        <div>
+          <ShowAppRating
+            ownApp={true}
+            voteAverage={rating ? rating.averageRating : undefined}
+            voteCount={rating ? rating.numberOfRatings : undefined}
+            className="pl-5 md:pl-0 md:pt-1"
+          />
+        </div>
+      </div>
+      <Dialog
+        open={showRatingEditor}
+        onOpenChange={() => setShowRatingEditor(false)}
+      >
+        <DialogContent className="z-[110] p-0">
+          <RatingEditor />
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
---
+++ src/components/movies/rating/ShowAppRating.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+
+import TmdbLogo from "@/assets/TMDB.png";
+import AppLogo from "@/assets/logo.png";
+import { cn, ratingColor } from "@/lib/utils";
+
+interface ShowAppRatingProps {
+  ownApp: boolean;
+  voteAverage: number | undefined;
+  voteCount: number | undefined;
+  className?: string;
+}
+
+export default function ShowAppRating({
+  ownApp,
+  voteAverage,
+  voteCount,
+  className,
+}: ShowAppRatingProps) {
+  return (
+    <div className={cn("flex items-center gap-4", className)}>
+      <div
+        className={`relative ${ownApp ? "h-8 w-8 md:h-12 md:w-12" : "h-10 w-10 md:h-14 md:w-14"} flex-shrink-0`}
+      >
+        <Image
+          src={ownApp ? AppLogo : TmdbLogo}
+          alt="TMDB logo"
+          fill
+          sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw"
+          className="rounded"
+        />
+      </div>
+      <div className="flex w-full flex-col items-center justify-center">
+        {voteAverage ? (
+          <div className="flex gap-1 text-xl font-semibold italic md:text-2xl">
+            <span className={ratingColor(voteAverage)}>
+              {voteAverage.toFixed(1)}
+            </span>
+            <span className="text-gray-400">/10</span>
+          </div>
+        ) : (
+          <span className="text-center text-xl text-foreground/40 md:text-2xl">
+            S/P
+          </span>
+        )}
+        <span className="text-xs text-foreground/40">
+          Votos: {voteCount ? voteCount : "-"}
+        </span>
+      </div>
+    </div>
+  );
+}
