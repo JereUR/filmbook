6b1b74b66c5dbcc0173b31141e86fe11e309a3c9
JereUR <jeremias.jdv@gmail.com>
2024-09-23 23:44:26 -0300

    feat: Obtener rating de usuario para una movie y si existe reemplazarla

--- src/components/movies/rating/RatingEditor.tsx
+++ src/components/movies/rating/RatingEditor.tsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Popcorn } from "lucide-react";
+import { useSession } from "@/app/(main)/SessionProvider";
+import { useQuery } from "@tanstack/react-query";
+import kyInstance from "@/lib/ky";
 
-import "./styles.css";
+interface RatingEditorProps {
+  movieId: string;
+}
 
-export default function RatingEditor() {
-  const [rating, setRating] = useState(0);
+export default function RatingEditor({ movieId }: RatingEditorProps) {
+  const [rating, setRating] = useState<number>(0);
   const [halfRating, setHalfRating] = useState(false);
+  const { user } = useSession();
+
+  const { data } = useQuery({
+    queryKey: ["rating", movieId, user?.id],
+    queryFn: () =>
+      kyInstance.get(`/api/movie/rating/${movieId}`).json<{rating:number | null}>(),
+    initialData: null,
+  });
+
+  useEffect(() => {
+    if (data?.rating) {
+      setRating(data.rating);
+    }
+  }, [data]);
 
   const handleClick = (index: number) => {
     if (rating === index + 1 && !halfRating) {
@@ -24,16 +43,16 @@ export default function RatingEditor() {
     const iconIndex = index + 1;
 
     if (rating >= iconIndex) {
-      return <Popcorn className="icon-thick text-primary h-6 w-6" />;
+      return <Popcorn className="icon-thick h-6 w-6 text-primary" />;
     } else if (rating === iconIndex - 0.5 && halfRating) {
       return (
         <div className="relative h-6 w-6 overflow-hidden">
-          <Popcorn className="absolute inset-0 text-primary clip-half-left icon-thick" />
-          <Popcorn className="absolute inset-0 text-gray-300 clip-half-right icon-thick" />
+          <Popcorn className="clip-half-left icon-thick absolute inset-0 text-primary" />
+          <Popcorn className="clip-half-right icon-thick absolute inset-0 text-gray-300" />
         </div>
       );
     } else {
-      return <Popcorn className="icon-thick text-gray-300 h-6 w-6" />;
+      return <Popcorn className="icon-thick h-6 w-6 text-gray-300" />;
     }
   };
 
