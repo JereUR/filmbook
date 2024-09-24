c069668c15018e26111766076909ce084cd648ab
JereUR <jeremias.jdv@gmail.com>
2024-09-24 14:08:15 -0300

    feat: manejar state de watclist

--- src/components/movies/rating/WatchlistButton.tsx
+++ src/components/movies/rating/WatchlistButton.tsx
@@ -1,12 +1,7 @@
-import {
-  QueryKey,
-  useMutation,
-  useQuery,
-  useQueryClient,
-} from "@tanstack/react-query";
-import { BookmarkPlus } from "lucide-react";
+import { QueryKey, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { BookmarkPlus, Loader2 } from "lucide-react";
 
-import { BookmarkInfo, WatchlistInfo } from "@/lib/types";
+import { WatchlistInfo } from "@/lib/types";
 import kyInstance from "@/lib/ky";
 import { useToast } from "@/components/ui/use-toast";
 import { cn } from "@/lib/utils";
@@ -21,27 +16,30 @@ export default function WatchlistButton({
   initialState,
 }: WatchlistButtonProps) {
   const { toast } = useToast();
-
   const queryClient = useQueryClient();
 
   const queryKey: QueryKey = ["watchlist-info", movieId];
 
-  const { data } = useQuery({
+  const { data: watchlistData, isLoading } = useQuery({
     queryKey,
     queryFn: () =>
       kyInstance.get(`/api/movie/watchlist/${movieId}`).json<WatchlistInfo>(),
-    initialData: initialState,
-    staleTime: Infinity,
+    initialData: initialState, 
+    staleTime: Infinity, 
   });
 
   const { mutate } = useMutation({
     mutationFn: () =>
-      data.isAddToWatchlistByUser
+      watchlistData?.isAddToWatchlistByUser
         ? kyInstance.delete(`/api/movie/watchlist/${movieId}`)
         : kyInstance.post(`/api/movie/watchlist/${movieId}`),
     onMutate: async () => {
       toast({
-        description: `Película ${data.isAddToWatchlistByUser ? "eliminada de watchlist" : "agregada a watchlist"}`,
+        description: `Película ${
+          watchlistData?.isAddToWatchlistByUser
+            ? "eliminada de tu watchlist"
+            : "agregada a tu watchlist"
+        }`,
       });
 
       await queryClient.cancelQueries({ queryKey });
@@ -54,29 +52,35 @@ export default function WatchlistButton({
 
       return { previousState };
     },
-    onError(error, variables, context) {
+    onError: (error, variables, context) => {
       queryClient.setQueryData(queryKey, context?.previousState);
       console.error(error);
       toast({
         variant: "destructive",
-        description: "Error al intentar agregar la película a tu watchlist.",
+        description: "Error al intentar actualizar tu watchlist.",
       });
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey });
     },
   });
 
   return (
     <div className="flex flex-col items-center">
-      <button onClick={() => mutate()} className="flex items-center gap-2 border-none">
-        <BookmarkPlus
-          className={cn(
-            "icon-fine h-10 w-10 cursor-pointer",
-            data.isAddToWatchlistByUser
-              ? "fill-primary text-background"
-              : "text-muted-foreground",
-          )}
-        />
-      </button>
-      <span className="mt-1 text-sm font-semibold">Watchlist</span>
+      <BookmarkPlus
+        className={cn(
+          "icon-fine h-10 w-10 cursor-pointer",
+          watchlistData?.isAddToWatchlistByUser
+            ? "fill-primary text-background"
+            : "text-muted-foreground",
+        )}
+        onClick={() => mutate()} 
+      />
+      {isLoading ? (
+        <Loader2 className="h-4 w-4 animate-spin text-primary" />
+      ) : (
+        <span className="mt-1 text-sm font-semibold">Watchlist</span>
+      )}
     </div>
   );
 }
