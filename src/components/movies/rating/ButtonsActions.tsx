0c80375c4e872b70134230107f9e63da21c62042
JereUR <jeremias.jdv@gmail.com>
2024-09-24 20:29:03 -0300

    feat: Botones para marcar como vista y como favorita una pelicula

--- src/components/movies/rating/ButtonsActions.tsx
+++ src/components/movies/rating/ButtonsActions.tsx
@@ -5,78 +5,70 @@ import { Eye, Heart, Loader2 } from "lucide-react";
 import kyInstance from "@/lib/ky";
 import WatchlistButton from "./WatchlistButton";
 import { useSession } from "@/app/(main)/SessionProvider";
-import { WatchlistInfo } from "@/lib/types";
+import WatchedButton from "./WatchedButton";
+import LikeButton from "./LikeButton";
+import { LikedInfo, WatchedInfo } from "@/lib/types";
 
 interface ButtonActionsProps {
   movieId: string;
-  watched: boolean;
-  setWatched: (watched: boolean) => void;
   watchlist: { userId: string; movieId: string }[];
+  reviews: {
+    userId: string;
+    movieId: string;
+    liked: boolean;
+    watched: boolean;
+  }[];
 }
 
 export default function ButtonActions({
   movieId,
-  watched,
-  setWatched,
   watchlist,
+  reviews,
 }: ButtonActionsProps) {
-  const [liked, setLiked] = useState<boolean>(false);
-
   const { user } = useSession();
 
-  const {
-    data: movieStates,
-    isLoading: isLoadingMovieStates,
-    isFetching: isFecthingMovieStates,
-  } = useQuery({
-    queryKey: ["movie-states", movieId, user?.id],
-    queryFn: () =>
-      kyInstance
-        .get(`/api/movie/watched/${movieId}`)
-        .json<{ watched: boolean; like: boolean }>(),
-    initialData: { watched: false, like: false },
-  });
+  const isWatchedByUser = reviews
+    ? reviews.some(
+        (movie) =>
+          movie.userId === user.id &&
+          movie.movieId === movieId &&
+          movie.watched
+      )
+    : false;
 
-  useEffect(() => {
-    if (movieStates && !isLoadingMovieStates) {
-      setWatched(movieStates.watched);
-      setLiked(movieStates.like);
-    }
-  }, [movieStates, isLoadingMovieStates]);
+  const isLikedByUser = reviews
+    ? reviews.some(
+        (movie) =>
+          movie.userId === user.id &&
+          movie.movieId === movieId &&
+          movie.liked,
+      )
+    : false;
+
+  const isInWatchlistByUser = watchlist
+    ? watchlist.some(
+        (movie) => movie.userId === user.id && movie.movieId === movieId,
+      )
+    : false;
 
   return (
     <div className="flex justify-around">
-      <div className="flex flex-col items-center">
-        <Eye
-          className={`icon-fine h-10 w-10 cursor-pointer ${watched ? "fill-primary text-background" : "text-muted-foreground"}`}
-          onClick={() => setWatched(!watched)}
-        />
-        {isLoadingMovieStates || isFecthingMovieStates ? (
-          <Loader2 className="h-4 w-4 animate-spin text-primary" />
-        ) : (
-          <span className="mt-1 text-sm font-semibold">Mirar</span>
-        )}
-      </div>
-      <div className="flex flex-col items-center">
-        <Heart
-          className={`icon-fine h-10 w-10 cursor-pointer ${liked ? "fill-red-500 text-background dark:fill-red-600" : "text-muted-foreground"}`}
-          onClick={() => setLiked(!liked)}
-        />
-        {isLoadingMovieStates || isFecthingMovieStates ? (
-          <Loader2 className="h-4 w-4 animate-spin text-primary" />
-        ) : (
-          <span className="mt-1 text-sm font-semibold">Like</span>
-        )}
-      </div>
+      <WatchedButton
+        movieId={movieId}
+        initialState={{
+          isWatchedByUser
+        }}
+      />
+      <LikeButton
+        movieId={movieId}
+        initialState={{
+          isLikedByUser,
+        }}
+      />
       <WatchlistButton
         movieId={movieId}
         initialState={{
-          isAddToWatchlistByUser: watchlist
-            ? watchlist.some(
-                (movie) =>
-                  movie.userId === user.id && movie.movieId === movieId,
-              )
-            : false,
+          isAddToWatchlistByUser: isInWatchlistByUser,
         }}
       />
     </div>
---
+++ src/components/movies/rating/LikeButton.tsx
@@ -0,0 +1,91 @@
+import {
+  QueryKey,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { Heart, Loader2 } from "lucide-react";
+
+import { LikedInfo } from "@/lib/types";
+import kyInstance from "@/lib/ky";
+import { useToast } from "@/components/ui/use-toast";
+import { cn } from "@/lib/utils";
+
+interface LikeButtonProps {
+  movieId: string;
+  initialState: LikedInfo;
+}
+
+export default function LikeButton({
+  movieId,
+  initialState,
+}: LikeButtonProps) {
+  const { toast } = useToast();
+  const queryClient = useQueryClient();
+
+  const queryKey: QueryKey = ["like-info", movieId];
+
+  const { data: likeData, isLoading } = useQuery({
+    queryKey,
+    queryFn: () =>
+      kyInstance.get(`/api/movie/liked/${movieId}`).json<LikedInfo>(),
+    initialData: initialState,
+    staleTime: Infinity,
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: () =>
+      likeData?.isLikedByUser
+        ? kyInstance.delete(`/api/movie/liked/${movieId}`)
+        : kyInstance.post(`/api/movie/liked/${movieId}`),
+    onMutate: async () => {
+      toast({
+        description: `Película ${
+          likeData?.isLikedByUser
+            ? "marcada como favorita"
+            : "desmarcada como favorita"
+        }`,
+      });
+
+      await queryClient.cancelQueries({ queryKey });
+
+      const previousState = queryClient.getQueryData<LikedInfo>(queryKey);
+
+      queryClient.setQueryData<LikedInfo>(queryKey, () => ({
+        isLikedByUser: !previousState?.isLikedByUser,
+      }));
+
+      return { previousState };
+    },
+    onError: (error, variables, context) => {
+      queryClient.setQueryData(queryKey, context?.previousState);
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Error al intentar actualizar tu watchlist.",
+      });
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey });
+    },
+  });
+
+  return (
+    <div className="flex flex-col items-center">
+      <Heart
+        className={cn(
+          "icon-fine h-10 w-10 cursor-pointer",
+          likeData?.isLikedByUser
+            ? "fill-red-500 dark:fill-red-600 text-background"
+            : "text-muted-foreground",
+        )}
+        onClick={() => mutate()}
+      />
+      {isLoading ? (
+        <Loader2 className="h-4 w-4 animate-spin text-primary" />
+      ) : (
+        <span className="mt-1 text-sm font-semibold">Me gusta</span>
+      )}
+    </div>
+  );
+}
