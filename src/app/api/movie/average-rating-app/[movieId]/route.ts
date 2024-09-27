f2f36628e22a7c6398b9dcfe4c9d81f38e6b26b7
JereUR <jeremias.jdv@gmail.com>
2024-09-27 19:22:56 -0300

    feat: Ruta para obtener puntaje de pelicula

---
+++ src/app/api/movie/average-rating-app/[movieId]/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { movieId: string } },
+) {
+  const rating = await prisma.movieRating.findFirst({
+    where: {
+      movieId: params.movieId,
+    },
+    select: {
+      averageRating: true,
+      numberOfRatings:true
+    },
+  });
+
+  if (!rating) {
+    return NextResponse.json(null);
+  }
+
+  return NextResponse.json(rating);
+}
