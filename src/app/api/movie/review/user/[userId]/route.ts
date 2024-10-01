7a696539632a6e8f12a2fb95de52a2f46a2080e2
JereUR <jeremias.jdv@gmail.com>
2024-09-30 22:50:15 -0300

    feat: Api para obtener todas las reviews de un usuario (Con paginacion incluida)

---
+++ src/app/api/movie/review/user/[userId]/route.ts
@@ -0,0 +1,55 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { validateRequest } from "@/auth";
+import { ReviewInfo } from "@/lib/types";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { userId: string } },
+) {
+  const { user: loggedInUser } = await validateRequest();
+
+  if (!loggedInUser) {
+    return NextResponse.json({ error: "No autorizado." }, { status: 401 });
+  }
+
+  const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+  const pageSize = 20;
+
+  const reviews = await prisma.review.findMany({
+    where: {
+      userId: params.userId,
+    },
+    select: {
+      id: true,
+      movieId: true,
+      liked: true,
+      watched: true,
+      rating: true,
+      review: true,
+      updatedAt: true,
+      createdAt: true,
+      movie: true,
+    },
+    take: pageSize + 1,
+    cursor: cursor ? { id: cursor } : undefined, 
+    orderBy: { createdAt: "desc" }, 
+  });
+
+  const nextCursor = reviews.length > pageSize ? reviews[pageSize].id : null;
+
+  const data: ReviewInfo[] = reviews.slice(0, pageSize).map((review) => ({
+    id: review.id,
+    movieId: review.movieId,
+    movie: review.movie,
+    userId: params.userId,
+    liked: review.liked,
+    watched: review.watched,
+    rating: review.rating,
+    review: review.review,
+    createdAt: review.createdAt,
+    updatedAt: review.updatedAt,
+  }));
+
+  return NextResponse.json({ reviews: data, nextCursor });
+}
