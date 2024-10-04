aa178b47e76b3dbcb39860dca174e4b341a0c639
JereUR <jeremias.jdv@gmail.com>
2024-10-03 21:39:11 -0300

    fix: SIntaxis fix

--- src/components/movies/CastMemberShow.tsx
+++ src/components/movies/CastMemberShow.tsx
@@ -3,13 +3,13 @@ import CircularImage from "./CircularImage";
 
 interface CastMemberShowProps {
   member: CastMember;
-  handleImageClick:(image:ImageInfo)=>void
+  handleImageClick: (image: ImageInfo) => void
 }
 
-export default function CastMemberShow({ member,handleImageClick}: CastMemberShowProps) {
+export default function CastMemberShow({ member, handleImageClick }: CastMemberShowProps) {
   return (
     <div className="flex items-center gap-2 md:gap-3 min-h-[70px]">
-      <CircularImage src={member.profilePath} alt={`${member.name} avatar`} size={40} handleImageClick={handleImageClick}/>
+      <CircularImage src={member.profilePath} alt={`${member.name} avatar`} size={40} handleImageClick={handleImageClick} />
       <div className="flex flex-col">
         <span className="text-sm text-muted-foreground md:text-base">
           {member.name}
