d1d041fe56f1d0e6095595a2bdb4dc6b8b58fca8
JereUR <jeremias.jdv@gmail.com>
2024-10-04 20:29:27 -0300

    feat: Api para manejo de likes en review agregada

---
+++ src/app/api/movie/review/[reviewId]/likes/route.ts
@@ -0,0 +1,158 @@
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { LikeInfo } from "@/lib/types";
+
+export async function GET(
+  req: Request,
+  { params: { reviewId } }: { params: { reviewId: string } },
+) {
+  try {
+    const { user: loggedInUser } = await validateRequest();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "No autorizado." }, { status: 401 });
+    }
+
+    const review = await prisma.review.findUnique({
+      where: { id: reviewId},
+      select: {
+        likes: {
+          where: {
+            userId: loggedInUser.id,
+          },
+          select: {
+            userId: true,
+          },
+        },
+        _count: {
+          select: {
+            likes: true,
+          },
+        },
+      },
+    });
+
+    if (!review) {
+      return Response.json({ error: "Review no encontrada." }, { status: 404 });
+    }
+
+    const data: LikeInfo = {
+      likes: review._count.likes,
+      isLikedByUser: !!review.likes.length,
+    };
+
+    return Response.json(data);
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      { error: "Error Interno del Servidor." },
+      { status: 500 },
+    );
+  }
+}
+
+export async function POST(
+  req: Request,
+  { params: { reviewId } }: { params: { reviewId: string } },
+) {
+  try {
+    const { user: loggedInUser } = await validateRequest();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "No autorizado." }, { status: 401 });
+    }
+
+    const review = await prisma.review.findUnique({
+      where: { id: reviewId},
+      select: {
+        userId: true,
+      },
+    });
+
+    if (!review) {
+      return Response.json({ error: "Review no encontrada." }, { status: 404 });
+    }
+
+    await prisma.$transaction([
+      prisma.like.upsert({
+        where: {
+          userId_reviewId: {
+            userId: loggedInUser.id,
+            reviewId,
+          },
+        },
+        create: { userId: loggedInUser.id, reviewId },
+        update: {},
+      }),
+      ...(loggedInUser.id !== review.userId
+        ? [
+            prisma.notification.create({
+              data: {
+                issuerId: loggedInUser.id,
+                recipientId: review.userId,
+                reviewId,
+                type: "LIKE",
+              },
+            }),
+          ]
+        : []),
+    ]);
+
+    return new Response();
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      { error: "Error Interno del Servidor." },
+      { status: 500 },
+    );
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params: { reviewId } }: { params: { reviewId: string } },
+) {
+  try {
+    const { user: loggedInUser } = await validateRequest();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "No autorizado." }, { status: 401 });
+    }
+
+    const review = await prisma.review.findUnique({
+      where: { id: reviewId },
+      select: {
+        userId: true,
+      },
+    });
+
+    if (!review) {
+      return Response.json({ error: "Review no encontrada." }, { status: 404 });
+    }
+
+    await prisma.$transaction([
+      prisma.like.deleteMany({
+        where: {
+          userId: loggedInUser.id,
+          reviewId,
+        },
+      }),
+      prisma.notification.deleteMany({
+        where: {
+          issuerId: loggedInUser.id,
+          recipientId: review.userId,
+          reviewId,
+          type: "LIKE",
+        },
+      }),
+    ]);
+
+    return new Response();
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      { error: "Error Interno del Servidor." },
+      { status: 500 },
+    );
+  }
+}
