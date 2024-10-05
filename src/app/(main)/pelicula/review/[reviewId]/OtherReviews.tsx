ccaf4d6077922f61c176a426e016e7e3a8d24fe6
JereUR <jeremias.jdv@gmail.com>
2024-10-05 03:21:05 -0300

    feat: utilizar api para obtener otras reviews con paginacion

--- src/app/(main)/pelicula/review/[reviewId]/OtherReviews.tsx
+++ src/app/(main)/pelicula/review/[reviewId]/OtherReviews.tsx
@@ -1,7 +1,57 @@
+'use client'
+
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Loader2 } from "lucide-react";
+
+import kyInstance from "@/lib/ky";
+import { ReviewResumeInfoPage } from "@/lib/types";
+
 interface OtherReviewsProps {
   movieId: string
+  reviewId: string
 }
+export default function OtherReviews({ movieId, reviewId }: OtherReviewsProps) {
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ["other-reviews", movieId],
+    queryFn: ({ pageParam }) =>
+      kyInstance
+        .get(
+          `/api/movie/review/others/${movieId}/${reviewId}`,
+          pageParam ? { searchParams: { cursor: pageParam } } : {},
+        )
+        .json<ReviewResumeInfoPage>(),
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+  });
 
-export default function OtherReviews({ movieId }: OtherReviewsProps) {
-  return <div className='p-2 md:p-5 bg-card rounded-2xl'><div className="text-xl font-bold">Otras reviews</div>{movieId}</div>
+  const reviews = data?.pages.flatMap((page) => page.reviews) || [];
+
+  if (status === "pending") {
+    return <Loader2 className="animate-spin mx-auto" />;
+  }
+
+  if (status === "success" && !reviews.length && !hasNextPage) {
+    return (
+      <p className="text-center text-muted-foreground">
+        Todavía nadie a posteado nada.
+      </p>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <p className="text-center text-destructive">
+        Ocurrió un error al cargar las publicaciones
+      </p>
+    );
+  }
+
+  return <div className='p-2 md:p-5 bg-card rounded-2xl'><div className="text-xl font-bold">Otras reviews</div>{reviews.map(review=>review.user?.username)}</div>
 }
