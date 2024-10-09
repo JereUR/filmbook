45f0cdab29529ea6d9f67365a998cff63856e203
JereUR <jeremias.jdv@gmail.com>
2024-10-08 21:22:39 -0300

    feat: Modal para editar review agregado

---
+++ src/components/movies/review/EditReviewDialog.tsx
@@ -0,0 +1,62 @@
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import ReviewEditor from "../rating/ReviewEditor";
+import WatchedButton from "../rating/WatchedButton";
+import LikeButton from "../rating/LikeButton";
+import { ReviewData } from "@/lib/types";
+import '../rating/styles.css'
+
+interface EditReviewDialogProps {
+  reviewState: ReviewData
+  movieId: string
+  activateRefresh: () => void
+  open: boolean;
+  onClose: () => void;
+}
+
+export default function EditReviewDialog({
+  reviewState,
+  movieId,
+  activateRefresh,
+  open,
+  onClose,
+}: EditReviewDialogProps) {
+
+  function handleOpenChange(open: boolean) {
+    if (!open) {
+      onClose();
+    }
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={handleOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Editar review</DialogTitle>
+        </DialogHeader>
+        <div className="flex justify-around">
+          <WatchedButton
+            movieId={movieId}
+            initialState={{
+              isWatchedByUser: reviewState.watched
+            }}
+            activateRefresh={activateRefresh}
+          />
+          <LikeButton
+            movieId={movieId}
+            initialState={{
+              isLikedByUser: reviewState.liked,
+            }}
+            activateRefresh={activateRefresh}
+          /></div>
+        <hr className="-my-1 h-[1px] border-none bg-primary/40" />
+        <ReviewEditor
+          movieId={movieId}
+          ownRating={reviewState.rating}
+          reviewText={reviewState.review}
+          activateRefresh={activateRefresh}
+          edit={true}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/components/movies/review/ReviewMoreButton.tsx
+++ src/components/movies/review/ReviewMoreButton.tsx
@@ -1,26 +1,55 @@
-import { useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { Edit2, MoreHorizontal, Trash2, Share2 } from "lucide-react";
 
-import { ReviewInfo } from "@/lib/types";
+import { ReviewInfo, ReviewData } from "@/lib/types";
 import DeleteReviewDialog from "./DeleteReviewDialog";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
 import { useSession } from "@/app/(main)/SessionProvider";
+import EditReviewDialog from "./EditReviewDialog";
+import { useRouter } from "next/navigation";
 
 interface ReviewMoreButtonProps {
   review: ReviewInfo;
+  reviewWasChanged: boolean
+  setReviewWasChanged: Dispatch<SetStateAction<boolean>>
   className?: string;
 }
 
 export default function ReviewMoreButton({
   review,
+  reviewWasChanged,
+  setReviewWasChanged,
   className,
 }: ReviewMoreButtonProps) {
-  const {user} = useSession()
-
+  const { user } = useSession()
+  const [reviewState, setReviewState] = useState<ReviewData>({
+    id: review.id,
+    rating: review.rating || null,
+    review: review.review || "",
+    watched: review.watched || false,
+    liked: review.liked || false,
+  })
+  const [showReviewEditor, setShowReviewEditor] = useState<boolean>(false)
   const [showDeleteDialog, setShowDeleteDialog] = useState<boolean>(false);
 
-  console.log({review})
+  const { movieId } = review
+  const router = useRouter()
+
+  async function fetchNewReview() {
+    const response = await fetch(`/api/movie/review/movie/${movieId}`)
+    const data = await response.json()
+
+    if (data as ReviewData) {
+      setReviewState(data)
+    }
+  }
+
+  useEffect(() => {
+    if (reviewWasChanged) {
+      fetchNewReview().then(() => setReviewWasChanged(false));
+    }
+  }, [reviewWasChanged]);
 
   return (
     <div>
@@ -40,6 +69,7 @@ export default function ReviewMoreButton({
             </span>
           </DropdownMenuItem>
           {user.id === review.userId && (<div><DropdownMenuItem
+            onClick={() => setShowReviewEditor(true)}
             className="cursor-pointer"
           >
             <span className="flex items-center gap-3 font-bold text-foreground">
@@ -63,6 +93,7 @@ export default function ReviewMoreButton({
         open={showDeleteDialog}
         onClose={() => setShowDeleteDialog(false)}
       />
+      <EditReviewDialog reviewState={reviewState} movieId={movieId || ''} activateRefresh={() => router.refresh()} onClose={() => setShowReviewEditor(false)} open={showReviewEditor} />
     </div>
   );
 }
