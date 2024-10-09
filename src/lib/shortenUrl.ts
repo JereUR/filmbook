afeef9d1f39757ac3eb7a83ef9a29a48e2d8ed9e
JereUR <jeremias.jdv@gmail.com>
2024-10-09 18:51:45 -0300

    feat: Acortar url con Rebrandly. Sin utilizar por limitaciones en version gratuita

---
+++ src/lib/shortenUrl.ts
@@ -0,0 +1,28 @@
+"use server";
+
+export async function shortenUrl(longUrl: string): Promise<string> {
+  const apiKey = process.env.REBRANDLY_API_KEY;
+
+  if (!apiKey) {
+    throw new Error("API Key de Rebrandly no está configurada.");
+  }
+
+  const response = await fetch("https://api.rebrandly.com/v1/links", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      apikey: apiKey,
+    },
+    body: JSON.stringify({
+      destination: longUrl,
+      domain: { fullName: "rebrand.ly" },
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Error al acortar la URL");
+  }
+
+  const data = await response.json();
+  return data.shortUrl;
+}
