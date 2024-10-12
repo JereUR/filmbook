98ff5951849ea84992b7324dccf1474b4d018a35
JereUR <jeremias.jdv@gmail.com>
2024-10-11 21:18:59 -0300

    feat: Pagina modificada para mostrar tanto publicaciones guardadas como watchlist

--- src/app/(main)/marcadores/page.tsx
+++ src/app/(main)/marcadores/page.tsx
@@ -2,19 +2,39 @@ import { Metadata } from "next";
 
 import Bookmarks from "./Bookmarks";
 import TrendsSidebar from "@/components/TrendsSidebar";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { validateRequest } from "@/auth";
+import Watchlist from "./Watchlist";
 
 export const metadata: Metadata = {
   title: "Marcadores",
 };
 
-export default function BookmarksPage() {
+export default async function BookmarksPage() {
+  const { user: loggedInUser } = await validateRequest();
+
+  if (!loggedInUser) {
+    return (
+      <p className="text-destructive">No estas autorizado a ver esta página.</p>
+    );
+  }
+
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
-        <div className="rounded-2xl bg-card p-5 shadow-sm">
-          <h1 className="text-center text-2xl font-bold">Marcadores</h1>
-        </div>
-        <Bookmarks />
+        <Tabs defaultValue="bookmarks" className='w-full'>
+          <TabsList className="rounded-md bg-card-child p-1 text-muted-foreground shadow-sm">
+            <TabsTrigger value="bookmarks" className="text-xs sm:text-sm">Marcadores</TabsTrigger>
+            <TabsTrigger value="watchlist" className="text-xs sm:text-sm">Watchlist</TabsTrigger>
+          </TabsList>
+          <TabsContent value="bookmarks">
+            <Bookmarks />
+          </TabsContent>
+          <TabsContent value="watchlist">
+            <Watchlist userId={loggedInUser.id} />
+          </TabsContent>
+        </Tabs>
+
       </div>
       <TrendsSidebar />
     </main>
