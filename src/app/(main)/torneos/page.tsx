42f872b3d4dc56fe5fae0e6f6984e5889c775b92
JereUR <jeremias.jdv@gmail.com>
2024-10-20 20:54:04 -0300

    feat: Pagina de torneos

---
+++ src/app/(main)/torneos/page.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next";
+
+import TournamentView from "@/components/tournaments/TournamentView";
+
+export const metadata: Metadata = {
+  title: "Torneos",
+};
+
+export default function TournamentPage() {
+  return (
+    <main className="flex w-full min-w-0 gap-5">
+      <div className="w-full min-w-0 space-y-5">
+        <TournamentView />
+      </div>
+    </main>
+  )
+}
