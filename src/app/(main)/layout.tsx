c6380afeca2a873a896c907abcb37f5a031aa77e
JereUR <jeremias.jdv@gmail.com>
2024-10-20 20:53:30 -0300

    fix: Sintaxis fix

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -24,12 +24,13 @@ export default async function Layout({
         <div className="mx-auto flex w-full max-w-[1500px] grow gap-5 p-2 md:p-5">
           <div className="sticky top-[8.25rem] flex flex-col gap-5">
             <MenuBar className="hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80 z-50" />
-            <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}><PopularMovies className="hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80 z-50" />
+            <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
+              <PopularMovies className="hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80 z-50" />
             </Suspense>
           </div>
           {children}
         </div>
-        <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden z-50" />
+        <MenuBar className="sticky bottom-0 flex w-full justify-center gap-3 border-t bg-card p-3 sm:hidden z-50" />
       </div>
     </SessionProvider>
   );
