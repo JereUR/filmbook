e6e71673062cbc967d5ca75e8892edf1e6cfc61c
JereUR <jeremias.jdv@gmail.com>
2024-10-24 21:33:02 -0300

    feat: Skeleton actualizado

--- src/components/user/lists/diary/DiariesLoadingSkeleton.tsx
+++ src/components/user/lists/diary/DiariesLoadingSkeleton.tsx
@@ -1,9 +1,11 @@
+import { Calendar } from "lucide-react"
+
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function DiariesLoadingSkeleton() {
   return (
-    <div className="w-full space-y-5 rounded-2xl bg-card p-5 shadow-sm">
-      <DiaryLoadingSkeleton />
+    <div className="w-full rounded-2xl bg-card p-5 shadow-sm">
+
       <DiaryLoadingSkeleton />
       <DiaryLoadingSkeleton />
     </div>
@@ -12,22 +14,14 @@ export default function DiariesLoadingSkeleton() {
 
 function DiaryLoadingSkeleton() {
   return (
-    <div className="flex flex-wrap gap-5 w-full">
-      <div className="flex gap-2 w-full md:gap-4 p-2 md:p-4 border rounded-2xl">
-        <Skeleton className="h-40 w-28" />
-        <div className='w-full'>
-          <div className="space-y-1.5">
-            <Skeleton className="h-4 w-52 rounded" />
-            <Skeleton className="h-4 w-60 rounded" />
-          </div>
-          <Skeleton className="h-12 w-full rounded-t rounded-r rounded-bl-none mt-4" />
-          <Skeleton className="h-4 w-40 rounded-b rounded-t-none" />
-          <div className='flex justify-between mt-6'>
-            <Skeleton className="h-10 w-48 rounded" />
-            <Skeleton className="h-10 w-40 rounded" />
-          </div>
-        </div>
+    <div>
+      <div className="flex items-center gap-2 w-full md:gap-4 p-2 md:p-4 border rounded-2xl mb-2 md:mb-4">
+        <Calendar className='text-muted' /> <Skeleton className="h-5 w-40" />
       </div>
+      <Skeleton className="h-4 w-24 ml-2 md:ml-4" />
+      <hr className='w-[95%] text-muted h-[1px] mx-auto md:my-2' />
+      <DiaryLoadingSkeleton />
+      <DiaryLoadingSkeleton />
     </div>
   );
 }
