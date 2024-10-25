37d91be5eeed8abfd5649d73539d2cbd423fcd96
JereUR <jeremias.jdv@gmail.com>
2024-10-24 22:00:41 -0300

    fix: Bucle recursivo solucionado

--- src/components/user/lists/diary/DiariesLoadingSkeleton.tsx
+++ src/components/user/lists/diary/DiariesLoadingSkeleton.tsx
@@ -5,14 +5,13 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function DiariesLoadingSkeleton() {
   return (
     <div className="w-full rounded-2xl bg-card p-5 shadow-sm">
-
-      <DiaryLoadingSkeleton />
-      <DiaryLoadingSkeleton />
+      <DiariesItemsLoadingSkeleton/>
+      <DiariesItemsLoadingSkeleton/>
     </div>
   );
 }
 
-function DiaryLoadingSkeleton() {
+function DiariesItemsLoadingSkeleton() {
   return (
     <div>
       <div className="flex items-center gap-2 w-full md:gap-4 p-2 md:p-4 border rounded-2xl mb-2 md:mb-4">
@@ -20,8 +19,24 @@ function DiaryLoadingSkeleton() {
       </div>
       <Skeleton className="h-4 w-24 ml-2 md:ml-4" />
       <hr className='w-[95%] text-muted h-[1px] mx-auto md:my-2' />
-      <DiaryLoadingSkeleton />
-      <DiaryLoadingSkeleton />
+      <DiaryItemLoadingSkeleton />
+      <DiaryItemLoadingSkeleton />
+    </div>
+  );
+}
+
+function DiaryItemLoadingSkeleton() {
+  return (
+    <div className="flex flex-wrap w-full p-2 md:p-4">
+      <div className="flex gap-2 w-full md:gap-4 p-2 md:p-4 bg-background rounded-2xl">
+        <Skeleton className="h-28 w-20" />
+        <div className='w-full'>
+          <div className="space-y-1.5">
+            <Skeleton className="h-5 w-52 rounded" />
+            <Skeleton className="h-5 w-24 rounded" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
