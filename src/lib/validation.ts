96c0fc3e1569f79796aa25ad99491881b3739d1e
JereUR <jeremias.jdv@gmail.com>
2024-10-26 18:03:58 -0300

    feat: Validación de schema al crear torneo

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,6 +1,6 @@
-import { z } from "zod";
+import { z } from "zod"
 
-const requiredString = z.string().trim().min(1, "Requerido");
+const requiredString = z.string().trim().min(1, "Requerido")
 
 export const signUpSchema = z.object({
   email: requiredString.email("Correo electrónico inválido."),
@@ -12,34 +12,34 @@ export const signUpSchema = z.object({
     8,
     "Contraseña debe tener al menos 8 caracteres.",
   ),
-});
+})
 
-export type SignUpValues = z.infer<typeof signUpSchema>;
+export type SignUpValues = z.infer<typeof signUpSchema>
 
 export const loginSchema = z.object({
   username: requiredString,
   password: requiredString,
-});
+})
 
-export type LoginValues = z.infer<typeof loginSchema>;
+export type LoginValues = z.infer<typeof loginSchema>
 
 export const createPostSchema = z.object({
   content: requiredString,
   mediaIds: z
     .array(z.string())
     .max(5, "No puede tener más de 5 archivos adjuntos."),
-});
+})
 
 export const updateUserProfileSchema = z.object({
   displayName: requiredString,
   bio: z.string().max(1000, "Debe tener como máximo 1000 caracteres"),
-});
+})
 
-export type UpdateUserProfileValues = z.infer<typeof updateUserProfileSchema>;
+export type UpdateUserProfileValues = z.infer<typeof updateUserProfileSchema>
 
 export const createCommentSchema = z.object({
   content: requiredString,
-});
+})
 
 export const createReviewSchema = z.object({
   rating: z
@@ -50,4 +50,9 @@ export const createReviewSchema = z.object({
     .max(7, "Rating debe ser como máximo 7"),
   movieId: requiredString,
   review: z.string().optional(),
-});
+})
+
+export const createTournamentSchema = z.object({
+  name: requiredString,
+  description: z.string().optional(),
+})
