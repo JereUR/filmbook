eb2b0f46871b51ff7c42e1ca6172e916a36ca733
JereUR <jeremias.jdv@gmail.com>
2024-10-29 21:29:22 -0300

    feat: Formulario para agregar torneo agregado

--- src/components/tournaments/editor/AddTournamentButton.tsx
+++ src/components/tournaments/editor/AddTournamentButton.tsx
@@ -1,13 +1,27 @@
+'use client'
+
 import { BadgePlusIcon } from 'lucide-react'
+import { useState } from 'react'
 
 import { Button } from '@/components/ui/button'
+import AddEditTournamentDialog from '../AddEditTournamentDialog'
+
+export interface InputProps {
+  name: string
+  description?: string
+  onEdit: boolean
+  id: string
+}
 
 export default function AddTournamentButton() {
+  const [openDialog, setOpenDialog] = useState<boolean>(false)
+
   return (
     <div>
-      <Button className='flex items-center gap-2 '>
+      <Button className='flex items-center gap-2 md:gap-4 ' onClick={() => setOpenDialog(true)}>
         <BadgePlusIcon /> Agregar torneo
       </Button>
+      <AddEditTournamentDialog openDialog={openDialog} setOpenDialog={setOpenDialog} />
     </div>
   )
 }
