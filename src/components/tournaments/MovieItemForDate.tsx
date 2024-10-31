03222e12902ab260141425bd22a35023e2543954
JereUR <jeremias.jdv@gmail.com>
2024-10-30 21:39:11 -0300

    feat: Buscador de pelicula al agregar fecha a torneo. SIn finalizar

---
+++ src/components/tournaments/MovieItemForDate.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import type { SearchMovie } from "@/lib/types"
+import { getYear } from "@/lib/utils"
+import noImage from "@/assets/no-image-film.jpg"
+import { Dispatch } from "react"
+
+interface MovieItemProps {
+  movie: SearchMovie
+  setSelectedMovieId: Dispatch<React.SetStateAction<string>>
+}
+
+export default function MovieItemForDate({ movie, setSelectedMovieId }: MovieItemProps) {
+  const { id, poster_path, title, release_date, genre_names } = movie
+
+  return (
+    <Link href={`/pelicula/${id}?title=${title}&date=${getYear(release_date)}`}>
+      <div className="flex h-full cursor-pointer items-start space-x-4 overflow-hidden rounded-2xl border p-5 shadow-lg hover:bg-card/70 transition-colors duration-300 ease-in-out">
+        <div className="relative h-40 w-28 flex-shrink-0">
+          <Image
+            src={
+              poster_path
+                ? poster_path
+                : noImage
+            }
+            alt={title}
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            className="rounded"
+          />
+        </div>
+        <div className="p-4">
+          <h2 className="mb-2 text-xl font-semibold">
+            {title} ({getYear(release_date)})
+          </h2>
+          <p className="text-gray-500">{genre_names.join(", ")}</p>
+        </div>
+      </div>
+    </Link>
+  )
+}
---
+++ src/components/tournaments/SearchMovieForDate.tsx
@@ -0,0 +1,96 @@
+"use client"
+
+import { Dispatch, useState } from "react"
+import { Loader2 } from "lucide-react"
+
+import type { SearchMovie, SearchMoviesResponse } from "@/lib/types"
+import { useToast } from "@/components/ui/use-toast"
+import SearchForm from "../movies/search/SearchForm"
+import MovieItemForDate from "./MovieItemForDate"
+
+interface SearchMovieForDateProps {
+  setSelectedMovieId:Dispatch<React.SetStateAction<string>>
+}
+
+export default function SearchMovieForDate({setSelectedMovieId}:SearchMovieForDateProps) {
+  const [searchTerm, setSearchTerm] = useState<string>("")
+  const [movies, setMovies] = useState<SearchMovie[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
+  const [page, setPage] = useState<number>(1)
+  const [hasMore, setHasMore] = useState<boolean>(false)
+
+  const { toast } = useToast()
+
+  const searchMovies = async (pageNumber: number = 1) => {
+    if (!searchTerm) return
+    setLoading(true)
+
+    try {
+      const response = await fetch(
+        `/api/tmdb/search-movies?title=${encodeURIComponent(searchTerm)}&page=${pageNumber}`,
+      )
+      const data: SearchMoviesResponse = await response.json()
+
+      if (data.error) {
+        setError(data.error)
+        toast({
+          variant: "destructive",
+          description: error,
+        })
+      } else {
+        setMovies((prevMovies) => [...prevMovies, ...data.movies])
+        setHasMore(data.nextPage !== null)
+        setError(null)
+      }
+    } catch (error) {
+      setError(`Error al obtener los datos: ${error}.`)
+      toast({
+        variant: "destructive",
+        description: `Error al obtener los datos: ${error}`,
+      })
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const handleLoadMore = () => {
+    const nextPage = page + 1
+    setPage(nextPage)
+    searchMovies(nextPage)
+  }
+
+  return (
+    <div className="container mx-auto p-4">
+      <SearchForm
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        searchMovies={() => {
+          setMovies([])
+          setPage(1)
+          searchMovies(1)
+        }}
+        loading={loading}
+      />
+      {error && <p className="text-center text-red-500">{error}</p>}
+      {movies.length > 0 && (
+        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
+          {movies.map((movie) => (
+            <MovieItemForDate key={movie.id} movie={movie} setSelectedMovieId={setSelectedMovieId}/>
+          ))}
+        </div>
+      )}
+      {loading && <Loader2 className="mx-auto animate-spin" />}
+      {hasMore && !loading && (
+        <div className="mt-4 text-center">
+          <button
+            onClick={handleLoadMore}
+            className="rounded bg-blue-500 px-4 py-2 text-white"
+          >
+            Cargar más
+          </button>
+        </div>
+      )}
+    </div>
+  )
+}
