9e26e5ebaf17392cce0ed8cb6365f132c446732b
JereUR <jeremias.jdv@gmail.com>
2024-10-30 21:39:42 -0300

    feat: Estilos agregados

--- src/components/tournaments/TournamentItem.tsx
+++ src/components/tournaments/TournamentItem.tsx
@@ -1,26 +1,36 @@
+'use client'
+
 import { TournamentData } from "@/lib/types";
 import TournamentMoreButton from "./TournamentMoreButton";
+import { dateFormat } from "@/lib/utils";
+import Link from "next/link";
 
 interface TournamentItemProps {
   tournament: TournamentData
   admin: boolean
 }
 
-export default async function TournamentItem({ tournament, admin }: TournamentItemProps) {
-  const { id, name, description, dates, participants, createdAt } = tournament;
+export default function TournamentItem({ tournament, admin }: TournamentItemProps) {
+  const { id, name, description, dates, participants, createdAt, endDate } = tournament;
 
   return (
-    <div>
-      <h2>{name}</h2>
-      <p>{description}</p>
-      <p>Fechas: {dates}</p>
-      <p>Participantes: {participants}</p>
-      <p>Creado: {new Date(createdAt).toLocaleString()}</p>
+    <Link href={`/torneos/${id}`} className="relative p-2 md:p-5 border border-primary/40 rounded-2xl cursor-pointer bg-background hover:bg-background/70 transition-colors duration-300 ease-in-out">
       {admin && (
         <TournamentMoreButton
           tournament={tournament}
+          className="absolute top-3 right-3"
         />
       )}
-    </div>
+      <div className="flex gap-2 justify-center items-end mb-2 md:mb-4">
+        <h2 className="text-lg md:text-xl font-semibold">{name}</h2>
+        <p className={`text-center text-xs md:text-sm font-medium ${endDate ? 'text-orange-500 dark:text-orange-600' : 'text-green-500 dark:text-green-600'}`}>{endDate ? 'Inactivo' : 'Activo'}</p>
+      </div>
+      <p className={`text-sm md:text-base text-muted-foreground/40 italic ${!description && 'text-center'}`}>{description ? description : 'Sin descripción'}</p>
+      <div className="flex justify-around py-2 md:py-4 my-3 md:my-6 border border-muted rounded-2xl">
+        <p>Fechas: {dates}</p>
+        <p>Participantes: {participants}</p>
+      </div>
+      <p className='text-light text-xs md:text-sm italic text-muted-foreground/40'>Creado: {dateFormat(createdAt.toISOString())}</p>
+    </Link>
   );
 }
