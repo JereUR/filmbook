4c27a099299cfefe009e6ef9b0ef0985ff5f7ed1
JereUR <jeremias.jdv@gmail.com>
2024-11-02 21:53:34 -0300

    feat: Formulario para agregar participante agregado

--- src/components/tournaments/participants/editor/AddParticipantButton.tsx
+++ src/components/tournaments/participants/editor/AddParticipantButton.tsx
@@ -1,13 +1,20 @@
+'use client'
+
 import { UserPlus } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
+import AddEditParticipantDialog from '../AddEditParticipantDialog'
+import { useState } from 'react'
 
 export default function AddParticipantButton() {
+  const [openDialog, setOpenDialog] = useState<boolean>(false)
+
   return (
     <div>
-      <Button className='flex items-center gap-2 bg-green-500 dark:bg-green-600 hover:bg-green-600 dark:hover:bg-green-700' >
+      <Button className='flex items-center gap-2 bg-green-500 dark:bg-green-600 hover:bg-green-600 dark:hover:bg-green-700' onClick={() => setOpenDialog(true)}>
         <UserPlus /> Agregar participante
       </Button>
+      <AddEditParticipantDialog openDialog={openDialog} setOpenDialog={setOpenDialog} />
     </div>
   )
 }
