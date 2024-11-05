f6cad91407fa93e7ead93da95579a398ea53a102
JereUR <jeremias.jdv@gmail.com>
2024-11-04 22:32:01 -0300

    feat: APi para obtener todos los participantes creados

---
+++ src/app/api/tournaments/participants/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest } from "next/server"
+
+import prisma from "@/lib/prisma"
+import { ParticipantsData } from "@/lib/types"
+
+export async function GET(req: NextRequest) {
+  try {
+    const participants = await prisma.participant.findMany({
+      orderBy: { name: "desc" },
+      select: {
+        id: true,
+        name: true,
+        username: true,
+      },
+    })
+
+    const data: ParticipantsData[] = participants.map((participant) => ({
+      id: participant.id,
+      name: participant.name,
+      username: participant.username,
+    }))
+
+    return Response.json(data)
+  } catch (error) {
+    return Response.json(
+      { error: "Error Interno del Servidor." },
+      { status: 500 },
+    )
+  }
+}
