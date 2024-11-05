d5db9fe0e445cd90a094509788eb627a995362b2
JereUR <jeremias.jdv@gmail.com>
2024-11-04 22:32:55 -0300

    fix: Archivo y componente renombrados

--- src/components/tournaments/TournamentsView.tsx
+++ src/components/tournaments/TournamentsView.tsx
@@ -5,7 +5,7 @@ import { validateAdmin } from '@/auth'
 import AddTournamentButton from './editor/AddTournamentButton'
 import TournamentsList from './TournamentsList'
 import AddParticipantButton from './participants/editor/AddParticipantButton'
-import AsignPointsButton from './points/editor/AsignPointsButton'
+import AssignPointsButton from './points/editor/AssignPointsButton'
 
 export default async function TournamentsView() {
   const { admin } = await validateAdmin()
@@ -13,10 +13,10 @@ export default async function TournamentsView() {
   return (
     <div className='w-full min-h-[80vh] flex flex-col gap-4 md:gap-10 p-2 md:p-5 rounded-2xl bg-card'>
       {admin &&
-        <div className='flex justify-end gap-2 md:gap-5'>
+        <div className='flex flex-col justify-center md:flex-row md:justify-end gap-2 md:gap-5'>
           <AddTournamentButton />
           <AddParticipantButton />
-          <AsignPointsButton />
+          <AssignPointsButton />
         </div>
       }
       <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
