a2ad76013b8885955cec6897572a567380db6ad2
JereUR <jeremias.jdv@gmail.com>
2024-11-06 21:00:51 -0300

    feat: Labels agregados

--- src/components/tournaments/points/editor/ParticipantPopover.tsx
+++ src/components/tournaments/points/editor/ParticipantPopover.tsx
@@ -5,6 +5,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import { InputAssignPointsProps, ParticipantsData } from "@/lib/types";
 import { Button } from "@/components/ui/button";
 import { Check, ChevronsUpDown } from "lucide-react";
+import { Label } from "@/components/ui/label";
 
 interface ParticipantPopoverProps {
   participants: ParticipantsData[]
@@ -35,54 +36,59 @@ export default function ParticipantPopover({ participants, loadingParticipants, 
   }
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger
-        asChild
-        className="border border-primary/40 bg-background text-xs hover:bg-background/50 md:text-sm z-[200]"
-      >
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="min-w-[250px] justify-between"
+    <div>
+      <Label htmlFor="dateInput" className="block text-md font-medium text-muted-foreground/40 mb-1">
+        Participante
+      </Label>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger
+          asChild
+          className="border border-primary/40 bg-background text-xs hover:bg-background/50 md:text-sm z-[200]"
         >
-          {participantIdSelected ?
-            participants.find((participant) => participant.id === participantIdSelected)?.name
-            : "Seleccione un participante"}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[250px] p-2 z-[200] border border-primary/40">
-        <Command className="max-h-[300px]">
-          <CommandInput placeholder="Buscar participante..." />
-          <CommandList className="scrollbar-thin overflow-auto">
-            <CommandEmpty>Sin resultados.</CommandEmpty>
-            <CommandGroup heading="Participantes">
-              {Array.isArray(participants) && participants.length > 0 ? (
-                participants.map((participant) => (
-                  <CommandItem key={participant.id} className={`cursor-pointer data-[selected='true']:bg-transparent`}>
-                    <Button
-                      variant="ghost"
-                      className="w-full justify-start text-left"
-                      onClick={() => handleParticipantClick(participant.id)}
-                    >
-                      <div className="flex items-center gap-2">
-                        {participant.name}
-                        {participantIdSelected === participant.id && (
-                          <Check className="ml-auto h-4 w-4 text-green-600" />
-                        )}
-                      </div>
-                    </Button>
-                  </CommandItem>
-                ))
-              ) : (
-                <CommandEmpty>Sin resultados.</CommandEmpty>
-              )}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="min-w-[300px] justify-between"
+          >
+            {participantIdSelected ?
+              participants.find((participant) => participant.id === participantIdSelected)?.name
+              : "Seleccione un participante"}
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[250px] p-2 z-[200] border border-primary/40">
+          <Command className="max-h-[300px]">
+            <CommandInput placeholder="Buscar participante..." />
+            <CommandList className="scrollbar-thin overflow-auto">
+              <CommandEmpty>Sin resultados.</CommandEmpty>
+              <CommandGroup heading="Participantes">
+                {Array.isArray(participants) && participants.length > 0 ? (
+                  participants.map((participant) => (
+                    <CommandItem key={participant.id} className={`cursor-pointer data-[selected='true']:bg-transparent`}>
+                      <Button
+                        variant="ghost"
+                        className="w-full justify-start text-left"
+                        onClick={() => handleParticipantClick(participant.id)}
+                      >
+                        <div className="flex items-center gap-2">
+                          {participant.name}
+                          {participantIdSelected === participant.id && (
+                            <Check className="ml-auto h-4 w-4 text-green-600" />
+                          )}
+                        </div>
+                      </Button>
+                    </CommandItem>
+                  ))
+                ) : (
+                  <CommandEmpty>Sin resultados.</CommandEmpty>
+                )}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </div>
   )
 
 }
--- src/components/tournaments/points/editor/TournamentParticipantPopovers.tsx
+++ src/components/tournaments/points/editor/TournamentParticipantPopovers.tsx
@@ -2,6 +2,7 @@
 
 import { Dispatch, useEffect, useState } from "react"
 import { Check, ChevronsUpDown, Loader2 } from "lucide-react"
+import Image from "next/image"
 
 import {
   Command,
@@ -16,7 +17,7 @@ import { Button } from "@/components/ui/button"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import { useToast } from "@/components/ui/use-toast"
 import noImage from '@/assets/no-image-film.jpg'
-import Image from "next/image"
+import { Label } from "@/components/ui/label"
 
 interface TournamentParticipantPopoverProps {
   participantId: string
@@ -73,11 +74,7 @@ export default function TournamentParticipantPopover({
     setOpenTournamentPopover(false)
   }
 
-
-  console.log(tournaments)
-
   const handleDateClick = (dateId: string, points: number, extraPoints: number) => {
-    console.log(points, extraPoints)
     setInput((prevInput) => ({
       ...prevInput,
       dateId,
@@ -89,7 +86,7 @@ export default function TournamentParticipantPopover({
 
   if (loadingParticipantData) {
     return (
-      <p className='flex items-center gap-2'><Loader2 className='text-center animate-spin' /> Buscando torneos en los que participa</p>
+      <p className='flex text-xs md:text-sm text-muted-foreground/40 items-center gap-2'><Loader2 className='animate-spin' /> Buscando torneos en los que participa</p>
     )
   }
 
@@ -101,106 +98,117 @@ export default function TournamentParticipantPopover({
         <p className="text-center text-primary">Sin torneos asignados</p>
       )}
       <div className='flex gap-4'>
-        <Popover open={openTournamentPopover} onOpenChange={setOpenTournamentPopover}>
-          <PopoverTrigger asChild>
-            <Button
-              variant="outline"
-              role="combobox"
-              aria-expanded={openTournamentPopover}
-              className="flex-1 justify-between"
-              disabled={noTournamentsAvailable}
-            >
-              {tournamentIdSelected
-                ? tournaments.find(tournament => tournament.tournamentId === tournamentIdSelected)?.tournamentName
-                : "Seleccione un torneo"}
-              <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-[250px] p-2 z-[200] border border-primary/40">
-            <Command className="max-h-[300px]">
-              <CommandInput placeholder="Buscar torneo..." />
-              <CommandList className="scrollbar-thin max-h-[300px] overflow-y-auto">
-                <CommandEmpty>Sin resultados.</CommandEmpty>
-                <CommandGroup heading="Torneos">
-                  {Array.isArray(tournaments) && tournaments.length > 0 ? (
-                    tournaments.map((tournament) => (
-                      <CommandItem
-                        key={tournament.tournamentId}
-                        onSelect={() => handleTournamentClick(tournament.tournamentId)}
-                        className={`cursor-pointer data-[selected='true']:bg-transparent`}
-                      >
-                        <div className="flex items-center gap-2">
-                          {tournament.tournamentName}
-                          {tournamentIdSelected === tournament.tournamentId && (
-                            <Check className="ml-auto h-4 w-4 text-green-600" />
-                          )}
-                        </div>
-                      </CommandItem>
-                    ))
-                  ) : (
-                    <CommandEmpty>Sin resultados.</CommandEmpty>
-                  )}
-                </CommandGroup>
-              </CommandList>
-            </Command>
-          </PopoverContent>
-        </Popover>
-        <Popover open={openDatePopover} onOpenChange={setOpenDatePopover}>
-          <PopoverTrigger asChild>
-            <Button
-              variant="outline"
-              role="combobox"
-              aria-expanded={openDatePopover}
-              className="flex-1 justify-between"
-              disabled={!tournamentIdSelected || noTournamentsAvailable}
-            >
-              {dateIdSelected
-                ? `Fecha ${selectedTournament.find((date) => date.dateId === dateIdSelected)?.date}`
-                : "Seleccione una fecha"}
-              <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-[250px] p-2 z-[200] border border-primary/40">
-            <Command className="max-h-[300px]">
-              <CommandInput placeholder="Buscar fecha..." />
-              <CommandList className="scrollbar-thin max-h-[300px] overflow-y-auto">
-                <CommandEmpty>Sin resultados.</CommandEmpty>
-                <CommandGroup heading="Fechas">
-                  {selectedTournament.length ? (
-                    selectedTournament.map((date) => (
-                      <CommandItem
-                        key={date.dateId}
-                        onSelect={() => handleDateClick(date.dateId, date.points, date.extraPoints)}
-                        className={`cursor-pointer data-[selected='true']:bg-transparent`}
-                      >
-                        <div className="flex items-center gap-2">
-                          {date.movie.posterPath && (
-                            <Image
-                              src={date.movie.posterPath || noImage}
-                              alt={date.movie.title}
-                              width={48}
-                              height={72}
-                              className="rounded"
-                            />
-                          )}
-                          <span>
-                            {date.movie.title} - Fecha {date.date}
-                          </span>
-                          {dateIdSelected === date.dateId && (
-                            <Check className="ml-auto h-4 w-4 text-green-600" />
-                          )}
-                        </div>
-                      </CommandItem>
-                    ))
-                  ) : (
-                    <CommandEmpty>Sin resultados.</CommandEmpty>
-                  )}
-                </CommandGroup>
-              </CommandList>
-            </Command>
-          </PopoverContent>
-        </Popover>
+        <div className="flex-1 justify-between">
+          <Label htmlFor="tournamentInput" className="block text-md font-medium text-muted-foreground/40 mb-1">
+            Torneo
+          </Label>
+          <Popover open={openTournamentPopover} onOpenChange={setOpenTournamentPopover}>
+
+            <PopoverTrigger asChild id="tournamentInput">
+              <Button
+                variant="outline"
+                role="combobox"
+                aria-expanded={openTournamentPopover}
+                className="w-full flex justify-between"
+                disabled={noTournamentsAvailable}
+              >
+                {tournamentIdSelected
+                  ? tournaments.find(tournament => tournament.tournamentId === tournamentIdSelected)?.tournamentName
+                  : "Seleccione un torneo"}
+                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[250px] p-2 z-[200] border border-primary/40">
+              <Command className="max-h-[300px]">
+                <CommandInput placeholder="Buscar torneo..." />
+                <CommandList className="scrollbar-thin max-h-[300px] overflow-y-auto">
+                  <CommandEmpty>Sin resultados.</CommandEmpty>
+                  <CommandGroup heading="Torneos">
+                    {Array.isArray(tournaments) && tournaments.length > 0 ? (
+                      tournaments.map((tournament) => (
+                        <CommandItem
+                          key={tournament.tournamentId}
+                          onSelect={() => handleTournamentClick(tournament.tournamentId)}
+                          className={`cursor-pointer data-[selected='true']:bg-transparent`}
+                        >
+                          <div className="flex items-center gap-2">
+                            {tournament.tournamentName}
+                            {tournamentIdSelected === tournament.tournamentId && (
+                              <Check className="ml-auto h-4 w-4 text-green-600" />
+                            )}
+                          </div>
+                        </CommandItem>
+                      ))
+                    ) : (
+                      <CommandEmpty>Sin resultados.</CommandEmpty>
+                    )}
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
+        </div>
+        <div className="flex-1 justify-between">
+          <Label htmlFor="dateInput" className="block text-md font-medium text-muted-foreground/40 mb-1">
+            Fecha
+          </Label>
+          <Popover open={openDatePopover} onOpenChange={setOpenDatePopover}>
+            <PopoverTrigger asChild id="dateInput">
+              <Button
+                className='w-full flex justify-between'
+                variant="outline"
+                role="combobox"
+                aria-expanded={openDatePopover}
+                disabled={!tournamentIdSelected || noTournamentsAvailable}
+              >
+                {dateIdSelected
+                  ? `Fecha ${selectedTournament.find((date) => date.dateId === dateIdSelected)?.date}`
+                  : "Seleccione una fecha"}
+                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[250px] p-2 z-[200] border border-primary/40">
+              <Command className="max-h-[300px]">
+                <CommandInput placeholder="Buscar fecha..." />
+                <CommandList className="scrollbar-thin max-h-[300px] overflow-y-auto">
+                  <CommandEmpty>Sin resultados.</CommandEmpty>
+                  <CommandGroup heading="Fechas">
+                    {selectedTournament.length ? (
+                      selectedTournament.map((date) => (
+                        <CommandItem
+                          key={date.dateId}
+                          onSelect={() => handleDateClick(date.dateId, date.points, date.extraPoints)}
+                          className={`cursor-pointer data-[selected='true']:bg-transparent`}
+                        >
+                          <div className="flex items-center gap-2">
+                            {date.movie.posterPath && (
+                              <Image
+                                src={date.movie.posterPath || noImage}
+                                alt={date.movie.title}
+                                width={48}
+                                height={72}
+                                className="rounded"
+                              />
+                            )}
+                            <span>
+                              {date.movie.title} - Fecha {date.date}
+                            </span>
+                            {dateIdSelected === date.dateId && (
+                              <Check className="ml-auto h-4 w-4 text-green-600" />
+                            )}
+                          </div>
+                        </CommandItem>
+                      ))
+                    ) : (
+                      <CommandEmpty>Sin resultados.</CommandEmpty>
+                    )}
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
+        </div>
       </div>
-    </div>
+    </div >
   )
 }
