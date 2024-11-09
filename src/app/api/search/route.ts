fcc957b00f80234ea9baeca5de7a04abcc81e49d
JereUR <jeremias.jdv@gmail.com>
2024-11-08 22:17:10 -0300

    fix: Errores de logica arreglados

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -15,9 +15,9 @@ export async function GET(req: NextRequest) {
 
     const { user } = await validateRequest()
 
-    /* if (!user) {
+    if (!user) {
       return Response.json({ error: "No autorizado." }, { status: 401 })
-    } */
+    }
 
     const posts = await prisma.post.findMany({
       where: {
@@ -43,7 +43,7 @@ export async function GET(req: NextRequest) {
           },
         ],
       },
-      include: getPostDataInclude(user ? user.id : null),
+      include: getPostDataInclude(user.id),
       orderBy: { createdAt: "desc" },
       take: pageSize + 1,
       cursor: cursor ? { id: cursor } : undefined,
