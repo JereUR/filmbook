fb4db56f27cd648222609479e633577dd3adff1e
JereUR <jeremias.jdv@gmail.com>
2024-11-09 18:52:02 -0300

    feat: Botones de inicio de sesion y registro agregados cuando no hay sesion iniciada

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link"
 import Image from "next/image"
+import { LogIn, UserRoundPlus } from "lucide-react"
 
 import SearchField from "@/components/SearchField"
 import UserButton from "@/components/UserButton"
 import logoImg from '@/assets/logo.png'
+import { validateRequest } from "@/auth"
+import { Button } from "@/components/ui/button"
 
-export default function Navbar() {
+export default async function Navbar() {
+  const { user } = await validateRequest()
+
   return (
     <header className="sticky top-0 z-[90] bg-card shadow-sm">
       <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
@@ -16,7 +21,19 @@ export default function Navbar() {
           </div>
         </Link>
         <SearchField />
-        <UserButton className="sm:ms-auto" />
+        {user ? <UserButton className="sm:ms-auto" /> :
+          <div className='flex flex-col gap-2 md:flex-row sm:ms-auto'><Button>
+            <Link href="/iniciar-sesion" className='flex items-center gap-2'>
+              <LogIn className="h-5 w-5" /> Iniciar Sesión
+            </Link>
+          </Button>
+            <Button className='bg-orange-500 dark:bg-orange-600 hover:bg-orange-600 dark:hover:bg-orange-700'>
+              <Link href="/registrarse" className='flex items-center gap-2'>
+                <UserRoundPlus className="h-5 w-5" /> Regístrate
+              </Link>
+            </Button>
+          </div>
+        }
       </div>
     </header>
   )
