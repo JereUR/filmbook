c4b12fd30ddfcecbe80f22922e6810fd47d24b4c
JereUR <jeremias.jdv@gmail.com>
2024-11-10 20:54:59 -0300

    feat: Funcionalidad de edicion de fecha gregada

---
+++ src/app/api/tournaments/dates/edit/route.ts
@@ -0,0 +1,126 @@
+import { validateAdmin } from "@/auth"
+import prisma from "@/lib/prisma"
+import { fetchMovieFromTMDB } from "@/lib/tmdb"
+import { Movie } from "@/lib/types"
+
+interface UpdateRequestBody {
+  dateId: string
+  date: number
+  tournamentId: string
+  movieId: string
+  visible: boolean
+}
+
+const BASE_IMG_TMDB = "https://image.tmdb.org/t/p/original"
+
+export async function PUT(req: Request) {
+  try {
+    const { user, admin } = await validateAdmin()
+
+    if (!admin || !user) {
+      return new Response(JSON.stringify({ error: "No autorizado." }), {
+        status: 401,
+      })
+    }
+
+    const { dateId, date, tournamentId, movieId, visible } =
+      (await req.json()) as UpdateRequestBody
+
+    const existingTournamentDate = await prisma.tournamentDate.findFirst({
+      where: { date, tournamentId },
+    })
+
+    if (existingTournamentDate && existingTournamentDate.id !== dateId) {
+      return new Response(JSON.stringify({ error: "La fecha ya existe." }), {
+        status: 404,
+      })
+    }
+
+    let existedMovie = await prisma.movie.findFirst({
+      where: { id: movieId },
+    })
+
+    if (!existedMovie) {
+      try {
+        const movieData = await fetchMovieFromTMDB(movieId)
+
+        const {
+          title,
+          backdrop_path,
+          poster_path,
+          release_date,
+          overview,
+          runtime,
+          vote_average,
+          vote_count,
+          production_companies,
+          spoken_languages,
+          production_countries,
+          genres,
+          directors,
+          crew,
+          cast,
+          providers,
+        } = movieData
+
+        const newMovie: Movie = {
+          id: movieId,
+          title: title,
+          backdropPath: backdrop_path,
+          posterPath: poster_path,
+          releaseDate: new Date(release_date),
+          overview: overview,
+          runtime: runtime,
+          voteAverage: vote_average,
+          voteCount: vote_count,
+          productionCompanies: production_companies,
+          spokenLanguages: spoken_languages,
+          productionCountries: production_countries,
+          genres: genres,
+          directors: directors?.map((director: any) => ({
+            id: director.id,
+            name: director.name,
+            profilePath: director.profile_path
+              ? `${BASE_IMG_TMDB}${director.profile_path}`
+              : null,
+            job: director.job,
+          })),
+          crew: crew,
+          cast: cast,
+          providers: providers,
+        }
+
+        existedMovie = await prisma.movie.create({
+          data: newMovie,
+        })
+      } catch (error) {
+        return new Response(
+          JSON.stringify({ error: "Error al obtener la película" }),
+          { status: 500 },
+        )
+      }
+    }
+
+    const updatedTournamentDate = await prisma.tournamentDate.update({
+      where: { id: dateId },
+      data: {
+        date,
+        tournamentId,
+        movieId,
+        visible,
+      },
+    })
+
+    return new Response(
+      JSON.stringify({ success: true, updatedTournamentDate }),
+      {
+        status: 200,
+      },
+    )
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: "Error Interno del Servidor." }),
+      { status: 500 },
+    )
+  }
+}
--- src/components/tournaments/dates/EditDateToTournamentDialog.tsx
+++ src/components/tournaments/dates/EditDateToTournamentDialog.tsx
@@ -1,6 +1,8 @@
 import { Dispatch, useState } from "react"
-import { useQueryClient } from "@tanstack/react-query"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import Image from "next/image"
 
+import './styles.css'
 import {
   Dialog,
   DialogContent,
@@ -9,13 +11,16 @@ import {
 } from "@/components/ui/dialog"
 import { Input } from '@/components/ui/input'
 import { Button } from "../../ui/button"
-import { InputDateTournamentProps } from "@/lib/types"
+import { DateForTournamentData, InputDateTournamentProps } from "@/lib/types"
 import SearchMovieForDate from "./SearchMovieForDate"
 import { Label } from "@/components/ui/label"
 import LoadingButton from "@/components/LoadingButton"
 import { useToast } from "@/components/ui/use-toast"
 import ErrorText from "@/components/ErrorText"
 import { initialState } from "./AddDateToTournamentDialog"
+import kyInstance from "@/lib/ky"
+import DatesPopover from "./DatePopover"
+import noImage from '@/assets/no-image-film.jpg'
 
 interface EditDateToTournamentDialogProps {
   tournamentId: string
@@ -23,15 +28,37 @@ interface EditDateToTournamentDialogProps {
   setOpenDialog: Dispatch<React.SetStateAction<boolean>>
 }
 
+export interface ShowMovie {
+  id: string
+  title: string
+  posterPath: string | null
+}
+
 export default function EditDateToTournamentDialog({ tournamentId, openDialog, setOpenDialog }: EditDateToTournamentDialogProps) {
   const [input, setInput] = useState<InputDateTournamentProps>(initialState)
-  const [selectedMovieId, setSelectedMovieId] = useState<string>("")
+  const [originalMovie, setOriginalMovie] = useState<ShowMovie | null>(null)
+  const [selectedMovieId, setSelectedMovieId] = useState<string | null>(null)
   const [loading, setLoading] = useState<boolean>(false)
   const [errorDate, setErrorDate] = useState<string | null>(null)
-  const { date } = input
+  const { dateId, date, visible } = input
 
   const { toast } = useToast()
   const queryClient = useQueryClient()
+
+  const {
+    data: dates,
+    isFetching,
+    status,
+  } = useQuery({
+    queryKey: ["dates", tournamentId],
+    queryFn: () =>
+      kyInstance
+        .get(
+          `/api/tournaments/${tournamentId}/dates`,
+        )
+        .json<DateForTournamentData[]>(),
+    initialData: [],
+  })
 
   function validations() {
     let error = null
@@ -52,31 +79,34 @@ export default function EditDateToTournamentDialog({ tournamentId, openDialog, s
     setLoading(true)
     try {
       const response = await fetch(`/api/tournaments/dates/edit`, {
-        method: 'POST',
+        method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ date, tournamentId, movieId: selectedMovieId }),
+        body: JSON.stringify({ dateId, date, tournamentId, movieId: selectedMovieId ? selectedMovieId : originalMovie?.id, visible }),
       })
       if (!response.ok) {
         const errorData = await response.json()
-        throw new Error(errorData.error || "Error al agregar la fecha")
+        throw new Error(errorData.error || "Error al editar la fecha")
       }
       toast({
-        description: "Fecha agregada al torneo.",
+        description: "Fecha del torneo editada.",
       })
 
       await queryClient.invalidateQueries({ queryKey: ["tournaments"] })
+      await queryClient.invalidateQueries({ queryKey: ["dates", tournamentId] })
 
       setOpenDialog(false)
       setInput(initialState)
+      setOriginalMovie(null)
+      setSelectedMovieId(null)
     } catch (error) {
       toast({
         variant: "destructive",
         description:
           error instanceof Error
             ? error.message
-            : "Error al agregar la fecha al torneo. Por favor vuelve a intentarlo.",
+            : "Error al editar la fecha al torneo. Por favor vuelve a intentarlo.",
       })
     } finally {
       setLoading(false)
@@ -87,29 +117,70 @@ export default function EditDateToTournamentDialog({ tournamentId, openDialog, s
     <Dialog open={openDialog} onOpenChange={setOpenDialog}>
       <DialogContent className="z-[150] min-w-[50vw] max-w-[900px] max-h-[600px] overflow-y-auto scrollbar-thin">
         <DialogHeader className='border-b border-primary/40 border-rounded'>
-          <DialogTitle className="text-center mb-4 text-xl font-semibold">AGREGAR FECHA</DialogTitle>
+          <DialogTitle className="text-center mb-4 text-xl font-semibold">EDITAR FECHA</DialogTitle>
         </DialogHeader>
         <div className="relative space-y-2 md:space-y-5">
-          <div>
-            <div className="flex gap-2 items-center">
-              <Label htmlFor="dateInput" className="block text-md font-medium text-gray-700 mb-1">
-                Número de Fecha
-              </Label>
-              {errorDate && <ErrorText errorText={errorDate} className="text-xs md:text-sm animate-pulse" />}
+          {dates && dates.length > 0 && <DatesPopover dates={dates} loading={isFetching} input={input} setInput={setInput} setOriginalMovie={setOriginalMovie} status={status} />}
+          <div className='flex gap-8 items-center'>
+            <div>
+              <div className="flex gap-2 items-center">
+                <Label htmlFor="dateInput" className="block text-md font-medium text-muted-foreground/40 mb-1">
+                  Número de Fecha
+                </Label>
+                {errorDate && <ErrorText errorText={errorDate} className="text-xs md:text-sm animate-pulse" />}
+              </div>
+              <Input
+                id="dateInput"
+                type="number"
+                value={date}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setInput({ ...input, date: parseInt(e.target.value) })
+                }
+                placeholder="Número de fecha"
+                className="w-[20vw] rounded-md border border-gray-300 py-2 px-3 focus:outline-none focus:ring-2 focus:ring-primary placeholder:text-muted-foreground/40 no-spinner"
+              />
             </div>
-            <Input
-              id="dateInput"
-              type="number"
-              value={date}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setInput({ ...input, date: parseInt(e.target.value) })
-              }
-              placeholder="Número de fecha"
-              className="w-[20vw] rounded-md border border-gray-300 py-2 px-3 focus:outline-none focus:ring-2 focus:ring-primary placeholder:text-muted-foreground/40 no-spinner"
-            />
+            <div>
+              <div className="flex gap-2 items-center">
+                <Label htmlFor="visibleInput" className="block text-md font-medium text-muted-foreground/40 mb-1">
+                  Visible?
+                </Label>
+                {errorDate && <ErrorText errorText={errorDate} className="text-xs md:text-sm animate-pulse" />}
+              </div>
+              <label className="switch-visible">
+                <input
+                  id="visibleInput"
+                  type="checkbox"
+                  checked={visible}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setInput({ ...input, visible: e.target.checked })
+                  }
+                  className="input-visible"
+                />
+                <span className="slider-visible"></span>
+              </label>
+            </div>
           </div>
+          {originalMovie &&
+            <div className='w-fit'>
+              <h2 className="block text-md font-medium text-muted-foreground/40 mb-1">Película actual</h2>
+              <div className="p-2 border rounded-2xl flex gap-4">
+                <div className="my-auto flex-none">
+                  <Image
+                    src={originalMovie.posterPath ? originalMovie.posterPath : noImage}
+                    alt={`${originalMovie.title} poster`}
+                    width={50}
+                    height={75}
+                    className="rounded"
+                  />
+                </div>
+                <h1 className="text-sm font-semibold">
+                  <span className="line-clamp-2 whitespace-pre-line">{originalMovie.title}</span>
+                </h1>
+              </div>
+            </div>}
           <div>
-            <Label htmlFor="movieInput" className="block text-md font-medium text-gray-700 mb-1">
+            <Label htmlFor="movieInput" className="block text-md font-medium text-muted-foreground/40 mb-1">
               Película
             </Label>
             <SearchMovieForDate selectedMovieId={selectedMovieId} setSelectedMovieId={setSelectedMovieId} />
@@ -119,16 +190,16 @@ export default function EditDateToTournamentDialog({ tournamentId, openDialog, s
               <LoadingButton
                 onClick={onSubmit}
                 loading={loading}
-                disabled={!date || !selectedMovieId}
-                className="bg-green-500 text-white hover:bg-green-600"
+                disabled={!dateId || !date}
+                className="bg-sky-500 text-foreground hover:bg-sky-600"
               >
-                Agregar
+                Editar
               </LoadingButton>
               <Button variant='outline' onClick={() => setOpenDialog(false)}>Cancelar</Button>
             </div>
           </div>
         </div>
       </DialogContent>
-    </Dialog>
+    </Dialog >
   )
 }
