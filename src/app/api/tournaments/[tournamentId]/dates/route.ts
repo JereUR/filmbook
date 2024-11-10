85f9e9faa37d8f9074610b2ce765c30a96ba5420
JereUR <jeremias.jdv@gmail.com>
2024-11-10 18:41:06 -0300

    feat: Api para obtener fechas de un torneo para edicion de la misma

---
+++ src/app/api/tournaments/[tournamentId]/dates/route.ts
@@ -0,0 +1,57 @@
+import { NextRequest, NextResponse } from "next/server"
+
+import prisma from "@/lib/prisma"
+import { validateAdmin } from "@/auth"
+import { DateForTournamentData } from "@/lib/types"
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { tournamentId: string } },
+): Promise<NextResponse<DateForTournamentData[] | { error: string }>> {
+  const { user, admin } = await validateAdmin()
+
+  if (!user || !admin) {
+    return NextResponse.json({ error: "No autorizado." }, { status: 401 })
+  }
+
+  try {
+    const dates = await prisma.tournament.findFirst({
+      where: { id: params.tournamentId },
+      select: {
+        dates: {
+          select: {
+            id: true,
+            date: true,
+            visible: true,
+            movie: {
+              select: {
+                id: true,
+                title: true,
+                posterPath: true,
+              },
+            },
+          },
+        },
+      },
+    })
+
+    const formattedData: DateForTournamentData[] =
+      dates?.dates.map((date) => ({
+        dateId: date.id,
+        date: date.date,
+        visible: date.visible,
+        movie: {
+          id: date.movie.id,
+          title: date.movie.title,
+          posterPath: date.movie.posterPath,
+        },
+      })) || []
+
+    return NextResponse.json(formattedData)
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error al obtener los datos del participante." },
+      { status: 500 },
+    )
+  }
+}
