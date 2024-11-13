3800d56fa2d64f644dd4b5a641844e2fa78172c8
JereUR <jeremias.jdv@gmail.com>
2024-11-12 22:49:33 -0300

    feat: Funcion para obtener informacion de torneo agregada

---
+++ src/lib/tournaments.ts
@@ -0,0 +1,20 @@
+import { Tournament } from "@/lib/types"
+
+export async function getTournamentById(
+  tournamentId: string,
+): Promise<Tournament | null> {
+  try {
+    console.log("test")
+    const response = await fetch(`/api/tournaments/${tournamentId}`)
+
+    if (!response.ok) {
+      throw new Error("Error al obtener los datos de la película")
+    }
+
+    const data: Tournament = await response.json()
+
+    return data
+  } catch (error) {
+    return null
+  }
+}
