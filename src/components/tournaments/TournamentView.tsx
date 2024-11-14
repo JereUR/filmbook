8790b468f66491c5669e24a4f971960560e83352
JereUR <jeremias.jdv@gmail.com>
2024-11-13 21:59:12 -0300

    feat: Vista mejorada

--- src/components/tournaments/TournamentView.tsx
+++ src/components/tournaments/TournamentView.tsx
@@ -1,12 +1,16 @@
 'use client'
 
 import { useEffect, useState } from "react"
-import { Loader2 } from "lucide-react"
+import { Loader2, Info, Calendar, Users } from 'lucide-react'
 
 import StandingsTable from "@/components/tournaments/StandingsTable"
 import { useToast } from "@/components/ui/use-toast"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Tournament } from "@/lib/types"
 import { getTournamentById } from "@/lib/tournaments"
+import DateItem from "./DateItem"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
+import Linkify from "../Linkify"
 
 interface TournamentViewProps {
   tournamentId: string
@@ -40,7 +44,6 @@ export default function TournamentView({ tournamentId }: TournamentViewProps) {
     getTournament()
   }, [tournamentId, toast])
 
-
   if (loadingTournament) {
     return <Loader2 className="mx-auto my-3 animate-spin" />
   }
@@ -56,9 +59,62 @@ export default function TournamentView({ tournamentId }: TournamentViewProps) {
   return (
     <main className="flex flex-col md:flex-row w-full min-w-0 gap-5">
       <div className="md:flex-grow md:w-3/4">
-        <StandingsTable tournamentId={tournamentId} standings={tournament.participants} />
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-5">
+            <Card className="w-full border-none">
+              <CardHeader>
+                <CardTitle className="text-3xl font-bold">{tournament.name}</CardTitle>
+                {tournament.description && (
+                  <CardDescription className="m-2 text-lg italic text-muted-foreground/40">
+                    <Linkify>
+                      {tournament.description}
+                    </Linkify>
+                  </CardDescription>
+                )}
+              </CardHeader>
+              <CardContent>
+                <div className="flex flex-wrap gap-4">
+                  <div className="flex items-center">
+                    <Users className="mr-2 h-5 w-5 text-muted-foreground" />
+                    <span className="text-sm font-medium">
+                      {tournament.participants.length} participantes
+                    </span>
+                  </div>
+                  <div className="flex items-center">
+                    <Calendar className="mr-2 h-5 w-5 text-muted-foreground" />
+                    <span className="text-sm font-medium">
+                      {tournament.dates.length} fechas disputadas
+                    </span>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+          <StandingsTable tournamentId={tournamentId} standings={tournament.participants} />
+        </div>
       </div>
-      <div className="md:w-1/4">
+      <div className="hidden md:block md:w-1/4 h-fit bg-card p-4 rounded-lg shadow-md">
+        <div className="mb-4">
+          <h2 className="text-2xl font-bold mb-2">Fechas disputadas</h2>
+          <div className="flex items-center text-sm text-muted-foreground/40">
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Info className="mr-2 h-4 w-4" />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p className='flex gap-2'>Las fechas con <div className="mt-[6px] w-2 h-2 rounded-full bg-primary" /> tienen puntos extras disponibles.</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            Fechas con puntos extras
+          </div>
+        </div>
+        <div className="space-y-3">
+          {tournament.dates.map((d) => (
+            <DateItem key={d.id} dateData={d} />
+          ))}
+        </div>
       </div>
     </main>
   )
