62c77bf6f2a8363bcfaae231b04729c533478fdc
JereUR <jeremias.jdv@gmail.com>
2024-11-16 21:05:15 -0300

    feat: Skeleton de reviews actualizado

--- src/components/user/lists/review/ReviewsLoadingSkeleton.tsx
+++ src/components/user/lists/review/ReviewsLoadingSkeleton.tsx
@@ -2,7 +2,13 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export default function ReviewsLoadingSkeleton() {
   return (
-    <div className="w-full space-y-2 rounded-2xl bg-card p-5 shadow-sm">
+    <div className="w-full space-y-5 rounded-2xl bg-card p-5 shadow-sm mt-5">
+      <div className='flex justify-between'>
+        <Skeleton className='h-10 w-96 mt-2' />
+        <div className="p-2 border rounded">
+          <Skeleton className="h-6 w-32 rounded" />
+        </div>
+      </div>
       <ReviewLoadingSkeleton />
       <ReviewLoadingSkeleton />
       <ReviewLoadingSkeleton />
