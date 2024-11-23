dab990716beb69c98a753e1800241d213e634573
JereUR <jeremias.jdv@gmail.com>
2024-11-22 21:35:41 -0300

    feat: Seccion de peliculas favoritas agregado

---
+++ src/components/user/lists/favoriteMovies/FavoriteMovies.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { useQuery } from '@tanstack/react-query'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import kyInstance from '@/lib/ky'
+import { FavoriteMovie } from "@/lib/types"
+
+interface FavoriteMoviesProps {
+  initialData: FavoriteMovie[]
+  username: string
+}
+
+export default function FavoriteMovies({ initialData, username }: FavoriteMoviesProps) {
+  const { data: favoriteMovies, isLoading, isError, error } = useQuery({
+    queryKey: ['favorite-movies', username],
+    queryFn: () =>
+      kyInstance.get(`/api/users/username/${username}/favorite-movies`).json<FavoriteMovie[]>(),
+    initialData,
+  })
+
+  if (isLoading) return <p>Cargando películas favoritas...</p>
+  if (isError) return <p>Error al cargar películas favoritas: {error?.message}</p>
+  if (favoriteMovies.length === 0) return null
+
+  return (
+    <div className="bg-card rounded-lg shadow-lg transition-all duration-300 hover:shadow-xl">
+      <h2 className="md:text-xl font-bold mb-4 text-primary/70">Películas favoritas</h2>
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 px-6">
+        {favoriteMovies.map((movie) => (
+          <Link
+            key={movie.id}
+            href={`/pelicula/${movie.movieId}?title=${encodeURIComponent(movie.movie.title)}&date=${movie.movie.releaseDate ? new Date(movie.movie.releaseDate).getFullYear() : ''
+              }`}
+            className="group block"
+          >
+            <div className="relative aspect-[2/3] rounded-lg overflow-hidden shadow-md transition-all duration-300 group-hover:shadow-xl group-hover:scale-105">
+              <Image
+                src={movie.movie.posterPath || '/placeholder-movie-poster.jpg'}
+                alt={movie.movie.title}
+                layout="fill"
+                objectFit="cover"
+                className="transition-all duration-300 group-hover:brightness-110"
+              />
+              <div className="absolute inset-0 bg-gradient-to-t from-card to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+              <div className="absolute bottom-0 left-0 right-0 p-3 text-foreground/70 transform translate-y-full group-hover:translate-y-0 transition-transform duration-300">
+                <p className="text-sm font-bold line-clamp-2 ">{movie.movie.title}</p>
+                {movie.movie.releaseDate && (
+                  <p className="text-xs mt-1 opacity-80">
+                    {new Date(movie.movie.releaseDate).getFullYear()}
+                  </p>
+                )}
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
