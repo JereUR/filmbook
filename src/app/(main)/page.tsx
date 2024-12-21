f4758c9c0fc65b4796c4e73d8916c842e2b80aba
JereUR <jeremias.jdv@gmail.com>
2024-12-20 22:46:14 -0300

    fix: Espacios arreglados

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -20,8 +20,8 @@ export default async function Home() {
   return (
     <main className="flex w-full min-w-0 gap-5">
       <Suspense fallback={<HomeSkeleton />}>
-        <div className="w-full min-w-0 space-y-2 md:space-y-5">
-          <div className="relative h-40 w-full group overflow-hidden rounded-2xl border-2 border-primary shadow-md md:hidden hover:shadow-lg transition-shadow duration-300">
+        <div className="w-full min-w-0">
+          <div className="relative h-40 w-full group overflow-hidden mb-2 rounded-2xl border-2 border-primary shadow-md md:hidden hover:shadow-lg transition-shadow duration-300">
             <Link
               href="/nominaciones-golden-globe"
               aria-label="Ir a sección de Golden Globe"
@@ -39,23 +39,25 @@ export default async function Home() {
               </div>
             </Link>
           </div>
-          {loggedInUser ? <div>
-            <PostEditor className="rounded-t-2xl border-b-0" />
-            <AddToDiaryButton />
-          </div> :
-            <UnauthorizedMessage sectionMessage='Necesitas iniciar sesión para realizar posteos y agregar nuevas películas vistas.' trendsSidebar={false} />}
-          <Tabs defaultValue="for-you">
-            <TabsList>
-              <TabsTrigger value="for-you">Para ti</TabsTrigger>
-              {loggedInUser && <TabsTrigger value="following">Seguidos</TabsTrigger>}
-            </TabsList>
-            <TabsContent value="for-you">
-              <ForYouFeed />
-            </TabsContent>
-            {loggedInUser && <TabsContent value="following">
-              <FollowingFeed />
-            </TabsContent>}
-          </Tabs>
+          <div className=' space-y-2 md:space-y-5'>
+            {loggedInUser ? <div>
+              <PostEditor className="rounded-t-2xl border-b-0" />
+              <AddToDiaryButton />
+            </div> :
+              <UnauthorizedMessage sectionMessage='Necesitas iniciar sesión para realizar posteos y agregar nuevas películas vistas.' trendsSidebar={false} />}
+            <Tabs defaultValue="for-you">
+              <TabsList>
+                <TabsTrigger value="for-you">Para ti</TabsTrigger>
+                {loggedInUser && <TabsTrigger value="following">Seguidos</TabsTrigger>}
+              </TabsList>
+              <TabsContent value="for-you">
+                <ForYouFeed />
+              </TabsContent>
+              {loggedInUser && <TabsContent value="following">
+                <FollowingFeed />
+              </TabsContent>}
+            </Tabs>
+          </div>
         </div>
       </Suspense>
       <Suspense fallback={<TrendSidebarSkeleton />}>
