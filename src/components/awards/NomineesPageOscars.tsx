301250fcb29caa4fcf337167eb303897486c3946
JereUR <jeremias.jdv@gmail.com>
2025-01-24 03:14:15 -0300

    feat: Titulo actualizado

--- src/components/awards/NomineesPageOscars.tsx
+++ src/components/awards/NomineesPageOscars.tsx
@@ -23,7 +23,7 @@ export default function NomineesPageOscars() {
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <div className="rounded-2xl bg-card p-5 shadow-sm">
-          <h1 className="text-center text-2xl font-bold">Nominaciones</h1>
+          <h1 className="text-center text-2xl font-bold">Nominaciones Oscars 97th Academy Awards</h1>
         </div>
         <div className='space-y-1'>
           <NominationsMovies handleImageClick={handleImageClick} nominationsMovie={nominationsMovie} />
