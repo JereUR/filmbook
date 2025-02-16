41454faa47c00020b17fb6310855d61f20554f3e
JereUR <jeremias.jdv@gmail.com>
2025-02-16 05:13:07 -0300

    feat: Page para setear predcciones agregada

---
+++ src/app/(main)/nominaciones-oscars/mis-predicciones/page.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next"
+
+import PredictionForm from "./PredictionForm"
+
+export const metadata: Metadata = {
+  title: "Mis Predicciones para los Oscars",
+}
+
+export default function PredictionsPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <PredictionForm />
+    </div>
+  )
+}
+
