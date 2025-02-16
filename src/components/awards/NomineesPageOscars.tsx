a04cfed800a1defc7416276b8e65ab11c73b7109
JereUR <jeremias.jdv@gmail.com>
2025-02-16 05:15:30 -0300

    feat: Mensaje de 'proximamente predicciones'

--- src/components/awards/NomineesPageOscars.tsx
+++ src/components/awards/NomineesPageOscars.tsx
@@ -1,17 +1,22 @@
-'use client'
+"use client"
 
-import { useState } from 'react'
+import { useState } from "react"
+/* import { useRouter } from "next/navigation" */
 
-import PhotoModal from '@/components/movies/PhotoModal'
-import { ImageInfo } from '@/lib/types'
-import NominationsMovies from './NominationsMovies'
-import NominationsPersons from './NominationsPersons'
-import NominationsSong from './NominationsSong'
-import useOscarsNominees from '@/hooks/useOscarsNominees'
+import PhotoModal from "@/components/movies/PhotoModal"
+import type { ImageInfo } from "@/lib/types"
+import NominationsMovies from "./NominationsMovies"
+import NominationsPersons from "./NominationsPersons"
+import NominationsSong from "./NominationsSong"
+import useOscarsNominees from "@/hooks/useOscarsNominees"
+/* import { Button } from "@/components/ui/button"
+import { useAuth } from "@/hooks/useAuth" */
 
 export default function NomineesPageOscars() {
   const [openModal, setOpenModal] = useState<boolean>(false)
   const [selectedImage, setSelectedImage] = useState<ImageInfo | null>(null)
+  /* const router = useRouter()
+  const { user } = useAuth() */
 
   const { nominationsPerson, nominationsMovie, nominationsOriginalSong } = useOscarsNominees()
 
@@ -19,23 +24,31 @@ export default function NomineesPageOscars() {
     setSelectedImage(image)
     setOpenModal(true)
   }
+
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <div className="rounded-2xl bg-card p-5 shadow-sm">
           <h1 className="text-center text-2xl font-bold">Nominaciones Oscars 97th Academy Awards</h1>
+          {/*  {user && (
+            <div className="mt-4 flex justify-center space-x-4">
+              <Button className='bg-primary hover:bg-primary/70' onClick={() => router.push("/mis-predicciones")}>Mis Predicciones</Button>
+              <Button className='bg-green-500 dark:bg-green-600 hover:bg-green-600 dark:hover:bg-green-700' onClick={() => router.push("/nominaciones-oscars/mis-predicciones")}>Agregar Predicciones</Button>
+            </div>
+          )} */}
+          <p className="text-sm text-muted-foreground text-center">
+            ¡Próximamente podrás hacer tus predicciones para los Oscars 2024! Prepárate para participar y comparte
+            tus elecciones con la comunidad. Las predicciones estarán disponibles hasta el 2 de marzo.
+          </p>
         </div>
-        <div className='space-y-1'>
+        <div className="space-y-1">
           <NominationsMovies handleImageClick={handleImageClick} nominationsMovie={nominationsMovie} />
           <NominationsPersons handleImageClick={handleImageClick} nominationsPerson={nominationsPerson} />
           <NominationsSong handleImageClick={handleImageClick} nominationsOriginalSong={nominationsOriginalSong} />
         </div>
       </div>
-      <PhotoModal
-        isOpen={openModal}
-        onClose={() => setOpenModal(false)}
-        image={selectedImage}
-      />
+      <PhotoModal isOpen={openModal} onClose={() => setOpenModal(false)} image={selectedImage} />
     </main>
   )
 }
+
