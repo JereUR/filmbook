fcddf873f73434cb0121aa0c5fb98200ea1de138
JereUR <jeremias.jdv@gmail.com>
2025-02-16 05:12:28 -0300

    feat: Page de 'mis predicciones' agregada

---
+++ src/app/(main)/mis-predicciones/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Mis predicciones",
+}
+
+export default function MyPredictionsPage() {
+  return (
+    <div>MyPredictionsPage</div>
+  )
+}
