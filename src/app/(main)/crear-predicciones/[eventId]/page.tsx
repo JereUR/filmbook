2eeef9f3d03258194eee939c09b21bb1f8f19061
JereUR <jeremias.jdv@gmail.com>
2025-02-23 20:54:15 -0300

    feat: Estilos actualizados

--- src/app/(main)/crear-predicciones/[eventId]/page.tsx
+++ src/app/(main)/crear-predicciones/[eventId]/page.tsx
@@ -14,9 +14,8 @@ export default async function PredictionsPage({ params }: { params: { eventId: s
   if (!user) {
     return <UnauthorizedMessage sectionMessage='Necesitas iniciar sesión para realizar predicciones.' trendsSidebar={false} />
   }
-
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-1 py-2 lg:p-5">
       <PredictionForm userId={user.id} eventId={params.eventId} />
     </div>
   )
