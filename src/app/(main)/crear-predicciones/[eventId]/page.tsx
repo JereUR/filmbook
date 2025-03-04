48fde5124de09ac5790fa1a84c5a93ff913f3c32
JereUR <jeremias.jdv@gmail.com>
2025-03-04 20:01:47 -0300

    feat: Funcion de crear predicciones deshabilitada

--- src/app/(main)/crear-predicciones/[eventId]/page.tsx
+++ src/app/(main)/crear-predicciones/[eventId]/page.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { validateRequest } from "@/auth"
 import PredictionForm from "@/components/predictions/PredictionForm"
 import UnauthorizedMessage from "@/components/UnauthorizedMessage"
+import { notFound } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: "Crear predicciones para los Oscars",
@@ -11,13 +12,15 @@ export const metadata: Metadata = {
 export default async function PredictionsPage({ params }: { params: { eventId: string } }) {
   const { user } = await validateRequest()
 
-  if (!user) {
+  return notFound()
+
+  /* if (!user) {
     return <UnauthorizedMessage sectionMessage='Necesitas iniciar sesión para realizar predicciones.' trendsSidebar={false} />
   }
   return (
     <div className="container mx-auto px-1 py-2 lg:p-5">
       <PredictionForm userId={user.id} eventId={params.eventId} />
     </div>
-  )
+  ) */
 }
 
